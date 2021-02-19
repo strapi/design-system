3d83724c4ca4de9ca874451cd868a8fcba74909f
mfrachet <marvin.frachet@strapi.io>
2021-02-19 16:31:28 +0100

    revert useTheme

--- src/Button/Button.stories.tsx
+++ src/Button/Button.stories.tsx
@@ -1,12 +1,8 @@
 import * as React from 'react';
 import { Meta } from '@storybook/react/types-6-0';
-
 import { Button } from './Button';
-import { useTheme } from '../ThemeProvider';
 
 export const Primary = () => {
-    const theme = useTheme();
-    console.log('theme', theme);
     return <Button>Hello world</Button>;
 };
 
