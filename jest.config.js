6652d9eb5ff660ec0a057ebb9d97d0fcca169ad0
mfrachet <marvin.frachet@strapi.io>
2021-02-23 09:43:54 +0100

    mid step

---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    testRegex: 'src/.*\\.spec\\.(tsx|js)$',
+};
---
+++ src/Button/Button.spec.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import { Button } from './Button';
+import { ThemeProvider } from '../ThemeProvider';
+import { lightTheme } from '../themes';
+
+describe('Button', () => {
+    it('match snapshots', () => {
+        const { container } = render(
+            <ThemeProvider theme={lightTheme}>
+                <Button>Hello world</Button>
+            </ThemeProvider>,
+        );
+
+        expect(container.firstChild).toMatchInlineSnapshot(`
+            <button
+              class="sc-bdfBwQ jeBhmY"
+            >
+              Hello world
+            </button>
+        `);
+    });
+});
