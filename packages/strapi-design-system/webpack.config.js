3649600972bae4aaeba1d6d8d20c1e955b12f1ed
mfrachet <marvin.frachet@strapi.io>
2021-03-24 15:01:16 +0100

    Minimal config for webpack

---
+++ packages/strapi-design-system/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const analyzePlugins = [];
+
+if (process.env.BUNDLE_ANALYZE) {
+  analyzePlugins.push(new BundleAnalyzerPlugin());
+}
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'production',
+  output: {
+    filename: 'strapi-ds-bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/(node_modules)/],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new CleanWebpackPlugin()].concat(analyzePlugins),
+  externals: {
+    react: 'react',
+    reactDOM: 'react-dom',
+  },
+};
