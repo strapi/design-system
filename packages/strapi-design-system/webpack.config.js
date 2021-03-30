cd493b53c588dc01f777ff958a8124af42eb5c12
Marvin Frachet <marvin.frachet@strapi.io>
2021-03-30 12:14:24 +0200

    Update packages/strapi-design-system/webpack.config.js

--- packages/strapi-design-system/webpack.config.js
+++ packages/strapi-design-system/webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
   mode: 'production',
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'umd',
   },
   module: {
