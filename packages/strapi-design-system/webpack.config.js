ec9923bfda46ad8eccbe7a2c7ba9eb5dae9ed85c
Marvin Frachet <marvin.frachet@strapi.io>
2021-03-30 13:58:53 +0200

    Add bundle per component (#30)

--- packages/strapi-design-system/webpack.config.js
+++ packages/strapi-design-system/webpack.config.js
@@ -1,7 +1,27 @@
+const fs = require('fs');
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
+// Allows to create distinct bundles in the dist folder
+// for people wanting to import only specific components such as
+// import Button from '@strapi/design-system/Button
+const excludedFolders = ['helpers', 'themes'];
+const fileNames = fs.readdirSync(path.resolve(__dirname, 'src'));
+const entry = fileNames
+  .filter((name) => !excludedFolders.includes(name))
+  .reduce((acc, curr) => {
+    if (curr.includes('.js')) {
+      acc[curr.replace('.js', '')] = path.resolve(__dirname, 'src');
+    } else {
+      // Folder resolution
+      acc[curr] = path.resolve(__dirname, 'src', curr);
+    }
+
+    return acc;
+  }, {});
+
+// Plugin section
 const analyzePlugins = [];
 
 if (process.env.BUNDLE_ANALYZE) {
@@ -9,10 +29,10 @@ if (process.env.BUNDLE_ANALYZE) {
 }
 
 module.exports = {
-  entry: './src/index.js',
+  entry,
   mode: 'production',
   output: {
-    filename: 'strapi-ds-bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'umd',
   },
