f6fc14594de5d6a4517ac7d4a719e061cf1c2a87
mfrachet <marvin.frachet@strapi.io>
2021-03-30 11:55:04 +0200

    Add spaces to theme

--- packages/strapi-design-system/src/themes/light-theme.js
+++ packages/strapi-design-system/src/themes/light-theme.js
@@ -4,4 +4,5 @@ export const lightTheme = {
   colors: tokens.color,
   shadows: tokens.shadow,
   zIndices: [5, 10, 15, 20], // TBD
+  spaces: ['0px', '4px', '8px', '12px', '16px', '20px', '24px', '32px', '40px', '48px', '56px', '64px'],
 };
