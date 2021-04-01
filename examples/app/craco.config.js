bd07139410bca35e9dd566858647993c48c2e1a0
soupette <cyril.lpz@gmail.com>
2021-04-01 08:50:58 +0200

    Add CRA test app
    Signed-off-by: soupette <cyril.lpz@gmail.com>

---
+++ examples/app/craco.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const alias = {
+  react: path.resolve(__dirname, 'node_modules', 'react'),
+  'react-dom': path.resolve(__dirname, 'node_modules', 'react-dom'),
+  'react/jsx-dev-runtime': path.resolve(__dirname, 'node_modules', 'react/jsx-dev-runtime'),
+  'react/jsx-runtime': path.resolve(__dirname, 'node_modules', 'react/jsx-runtime'),
+};
+
+module.exports = {
+  webpack: {
+    configure: (wConfig) => {
+      wConfig.resolve.alias = { ...wConfig.resolve.alias, ...alias };
+
+      return wConfig;
+    },
+  },
+};
--- examples/app/src/App.js
+++ examples/app/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Button as Button } from '@strapi/design-system/dist/Button';
+import { ThemeProvider, Grid, Area } from '@strapi/design-system';
 import { lightTheme as theme } from '@strapi/design-system/src/themes/light-theme';
-import { Button as Button1 } from '@buffetjs/styles';
-import './App.css';
-
-const version = React.version;
 
 function App() {
-  console.log({ Button });
-  console.log({ Button1 });
-  // return null;
   return (
-    <Button theme={theme}>
-      {/* <Grid
+    <ThemeProvider theme={theme}>
+      <Grid
         areas={[
           ['first', 'second', 'third'],
           ['last', 'last', 'last'],
         ]}
         cols="1fr 1fr 1fr"
         rows="1fr 1fr"
+        borderRadius="200px"
         background="primary700"
         gap={4}
         margin={8}
@@ -37,8 +30,8 @@ function App() {
         <Area name="last" background="neutral100" color="primary700">
           Last
         </Area>
-      </Grid> */}
-    </Button>
+      </Grid>
+    </ThemeProvider>
   );
 }
 
