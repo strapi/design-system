33cb87966a3babd55012a909db5b751b30846be6
mfrachet <marvin.frachet@strapi.io>
2021-04-07 11:36:47 +0200

    Preparing the fields

---
+++ packages/strapi-design-system/src/Text/__tests__/Text.spec.js
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import { Text } from '../Text';
+import { ThemeProvider } from '../../ThemeProvider';
+import { lightTheme } from '../../themes';
+
+describe('Text', () => {
+  it('snapshots the component', () => {
+    const { container } = render(
+      <ThemeProvider theme={lightTheme}>
+        <div>
+          <Text variant="header-1" as="h2">
+            Header 1
+          </Text>
+          <Text variant="header-2">Header 2</Text>
+          <Text variant="header-3">Header 3</Text>
+          <Text variant="subtitle">Subtitle</Text>
+          <Text variant="body">Body</Text>
+          <Text variant="body-highlight">Body highlight</Text>
+          <Text variant="button-text">Button text</Text>
+          <Text variant="button-text-s">Button text S</Text>
+          <Text variant="text-s">Text S</Text>
+          <Text variant="table-label">Table label</Text>
+        </div>
+      </ThemeProvider>,
+    );
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      <div>
+        <h2
+          class="sc-bdfBwQ bJSPzh"
+        >
+          Header 1
+        </h2>
+        <h2
+          class="sc-gsTCUz egfSDB"
+        >
+          Header 2
+        </h2>
+        <h3
+          class="sc-dlfnbm htIfAV"
+        >
+          Header 3
+        </h3>
+        <p
+          class="sc-hKgILt frQRKo"
+        >
+          Subtitle
+        </p>
+        <p
+          class="sc-eCssSg eVkiln"
+        >
+          Body
+        </p>
+        <p
+          class="sc-jSgupP hYIGFL"
+        >
+          Body highlight
+        </p>
+        <p
+          class="sc-gKsewC keEVsD"
+        >
+          Button text
+        </p>
+        <p
+          class="sc-iBPRYJ fEoQAx"
+        >
+          Button text S
+        </p>
+        <p
+          class="sc-fubCfw hXzLAV"
+        >
+          Text S
+        </p>
+        <p
+          class="sc-pFZIQ fartGS"
+        >
+          Table label
+        </p>
+      </div>
+    `);
+  });
+});
---
+++ packages/strapi-design-system/src/Text/components.js
@@ -0,0 +1,87 @@
+import styled from 'styled-components';
+
+// TODO: modify when necessary
+const fontFamily = `-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;`;
+
+const handleColor = ({ theme, color }) => theme.colors[color];
+
+export const Header1 = styled.h1`
+  font-family: ${fontFamily};
+  font-weight: 600;
+  font-size: ${32 / 16}rem;
+  line-height: 1.25;
+  color: ${handleColor};
+`;
+
+export const Header2 = styled.h2`
+  font-family: ${fontFamily};
+  font-weight: 600;
+  font-size: ${18 / 16}rem;
+  line-height: 1.22;
+  color: ${handleColor};
+`;
+
+export const Header3 = styled.h3`
+  font-family: ${fontFamily};
+  font-weight: 500;
+  font-size: 1rem;
+  line-height: 1.25;
+  color: ${handleColor};
+`;
+
+export const Subtitle = styled.p`
+  font-family: ${fontFamily};
+  font-weight: 400;
+  font-size: 1rem;
+  line-height: 1.5;
+  color: ${handleColor};
+`;
+
+export const Body = styled.p`
+  font-family: ${fontFamily};
+  font-weight: 400;
+  font-size: ${14 / 16}rem;
+  line-height: 1.43;
+  color: ${handleColor};
+`;
+
+export const BodyHighlight = styled.p`
+  font-family: ${fontFamily};
+  font-weight: 500;
+  font-size: ${14 / 16}rem;
+  line-height: 1.43;
+  color: ${handleColor};
+`;
+
+export const ButtonText = styled.p`
+  font-family: ${fontFamily};
+  font-weight: 600;
+  font-size: ${14 / 16}rem;
+  line-height: 1.14;
+  color: ${handleColor};
+`;
+
+export const SmallButtonText = styled.p`
+  font-family: ${fontFamily};
+  font-weight: 500;
+  font-size: ${12 / 16}rem;
+  line-height: 1.33;
+  color: ${handleColor};
+`;
+
+export const SmallText = styled.p`
+  font-family: ${fontFamily};
+  font-weight: 400;
+  font-size: ${12 / 16}rem;
+  line-height: 1.33;
+  color: ${handleColor};
+`;
+
+export const TableLabel = styled.p`
+  font-family: ${fontFamily};
+  font-weight: 600;
+  font-size: ${11 / 16}rem;
+  line-height: 1.45;
+  text-transform: uppercase;
+  color: ${handleColor};
+`;
