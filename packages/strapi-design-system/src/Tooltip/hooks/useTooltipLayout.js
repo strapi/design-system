fc5fe126b585eab5cec6891c4aa3cc5cad23eded
mfrachet <marvin.frachet@strapi.io>
2021-04-09 15:30:00 +0200

    Fixing stuff

--- packages/strapi-design-system/src/Tooltip/hooks/useTooltipLayout.js
+++ packages/strapi-design-system/src/Tooltip/hooks/useTooltipLayout.js
@@ -13,8 +13,8 @@ export const useTooltipLayout = (visible) => {
       const toggleSourceRect = toggleSource.getBoundingClientRect();
 
       const widthDifference = (tooltipRect.width - toggleSourceRect.width) / 2;
-      const nextLeft = toggleSourceRect.x - widthDifference;
-      const nextTop = toggleSourceRect.y - tooltipRect.height - 8;
+      const nextLeft = toggleSourceRect.left - widthDifference;
+      const nextTop = toggleSourceRect.top - tooltipRect.height - 8 + window.pageYOffset;
 
       tooltip.style.left = `${nextLeft}px`;
       tooltip.style.top = `${nextTop}px`;
