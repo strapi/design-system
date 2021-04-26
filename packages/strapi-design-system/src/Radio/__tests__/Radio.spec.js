caf681af86e4afea2d603ecc82705bca575b59c8
mfrachet <marvin.frachet@strapi.io>
2021-04-26 12:33:31 +0200

    Adding tests

--- packages/strapi-design-system/src/Radio/__tests__/Radio.spec.js
+++ packages/strapi-design-system/src/Radio/__tests__/Radio.spec.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
-import { Radio } from '../Radio';
+import { Radio, RadioGroup } from '../';
 import { ThemeProvider } from '../../ThemeProvider';
 import { lightTheme } from '../../themes';
 
@@ -8,10 +8,43 @@ describe('Radio', () => {
   it('snapshots the component', () => {
     const { container } = render(
       <ThemeProvider theme={lightTheme}>
-        <Radio />
+        <RadioGroup labelledBy="trophy-champions" onSelect={() => null} value={'pizza'}>
+          <Radio id="pizza" value="pizza">
+            Pizza
+          </Radio>
+        </RadioGroup>
       </ThemeProvider>,
     );
 
-    expect(container.firstChild).toMatchInlineSnapshot(``);
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      <div
+        aria-labelledby="trophy-champions"
+        role="radiogroup"
+      >
+        <div
+          class="sc-pFZIQ frprlu"
+        >
+          <input
+            aria-checked="true"
+            class="sc-bdfBwQ buUKqr"
+            id="pizza"
+            name="5d64adca-c5a2-4e1c-b12c-f691b6c87d34"
+            tabindex="0"
+            type="radio"
+            value="pizza"
+          />
+          <div
+            class="sc-fubCfw lkaSCX"
+          >
+            <label
+              class="sc-eCssSg geiiEt"
+              for="pizza"
+            >
+              Pizza
+            </label>
+          </div>
+        </div>
+      </div>
+    `);
   });
 });
