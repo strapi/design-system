2556088ffd24cfbecd75edc62ad1b8c453f0f095
mfrachet <marvin.frachet@strapi.io>
2021-04-30 09:10:22 +0200

    Add Row layout

--- packages/strapi-design-system/src/Row/Row.js
+++ packages/strapi-design-system/src/Row/Row.js
@@ -3,22 +3,22 @@ import styled from 'styled-components';
 
 export const Row = styled.div`
   display: flex;
-  flex-direction: ${({ reverse }) => reverse};
+  flex-direction: ${({ reverse }) => (reverse ? 'row-reverse' : 'row')};
   justify-content: ${({ justifyContent }) => justifyContent};
   align-items: ${({ alignItems }) => alignItems};
   flex-wrap: ${({ wrap }) => wrap};
 `;
 
 Row.defaultProps = {
-  reverse: false,
-  justifyContent: undefined,
   alignItems: 'center',
+  justifyContent: undefined,
+  reverse: false,
   wrap: undefined,
 };
 
 Row.propTypes = {
-  reverse: PropTypes.bool,
-  justifyContent: PropTypes.string,
   alignItems: PropTypes.string,
+  justifyContent: PropTypes.string,
+  reverse: PropTypes.bool,
   wrap: PropTypes.string,
 };
