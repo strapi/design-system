f9c749435c90df206d99e3012c0525e8c97d1546
mfrachet <marvin.frachet@strapi.io>
2021-05-03 11:57:14 +0200

    Remove unused visually hidden

--- packages/strapi-design-system/src/ProgressBar/ProgressBar.js
+++ packages/strapi-design-system/src/ProgressBar/ProgressBar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { VisuallyHidden } from '../VisuallyHidden';
 
 const ProgressBarSmall = styled.div`
   background: ${({ theme }) => theme.colors.neutral600};
