901567ef5ff6c5ffa379441695f02cdd2d9d20b1
mfrachet <marvin.frachet@strapi.io>
2021-05-04 08:17:52 +0200

    Add Tag component

---
+++ packages/strapi-design-system/src/Tag/Tag.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Text } from '../Text';
+import { Box } from '../Box';
+import { Row } from '../Row';
+
+const TagWrapper = styled(Box)`
+  border: 1px solid ${({ theme }) => theme.colors.primary200};
+  border-radius: ${({ theme }) => theme.borderRadius};
+
+  svg path {
+    fill: ${({ theme }) => theme.colors.primary600};
+  }
+`;
+
+const TagText = styled(Text)`
+  border-right: 1px solid ${({ theme }) => theme.colors.primary200};
+  padding-right: ${({ theme }) => theme.spaces[2]};
+`;
+
+export const Tag = ({ children, icon, ...props }) => (
+  <TagWrapper
+    as="button"
+    background="primary100"
+    color="primary600"
+    paddingBottom={2}
+    paddingTop={2}
+    paddingLeft={3}
+    paddingRight={3}
+    {...props}
+  >
+    <Row>
+      <TagText small={true} highlighted={true} as="span">
+        {children}
+      </TagText>
+
+      <Box paddingLeft={2}>
+        <Row>{icon}</Row>
+      </Box>
+    </Row>
+  </TagWrapper>
+);
+
+Tag.displayName = Tag;
+
+Tag.propTypes = {
+  children: PropTypes.string.isRequired,
+  icon: PropTypes.element.isRequired,
+};
