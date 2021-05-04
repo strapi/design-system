b0253a5b6b0baf8ae7f8be8f412338a4d5068424
Marvin Frachet <marvin.frachet@strapi.io>
2021-05-04 09:24:40 +0200

    Add Tag component (#55)

---
+++ packages/strapi-design-system/src/Tag/__tests__/Tag.e2e.js
@@ -0,0 +1,13 @@
+import { injectAxe, checkA11y } from 'axe-playwright';
+
+describe('Tag', () => {
+  beforeEach(async () => {
+    // This is the URL of the Storybook Iframe
+    await page.goto('http://localhost:6006/iframe.html?id=tag--base&viewMode=story');
+    await injectAxe(page);
+  });
+
+  it('triggers axe on the document', async () => {
+    await checkA11y(page);
+  });
+});
