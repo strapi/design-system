47d4c16813223ddaa730f463d58e5d91ec467908
mfrachet <marvin.frachet@strapi.io>
2021-05-10 14:56:25 +0200

    Fix popover tests

--- packages/strapi-design-system/src/Popover/__tests__/Popover.spec.js
+++ packages/strapi-design-system/src/Popover/__tests__/Popover.spec.js
@@ -1,17 +1,81 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import { Popover } from '../Popover';
 import { ThemeProvider } from '../../ThemeProvider';
 import { lightTheme } from '../../themes';
 
 describe('Popover', () => {
-  it('snapshots the component', () => {
-    const { container } = render(
+  it('snapshots the component', async () => {
+    const source = document.createElement('div');
+    source.innerText = 'Hello source';
+    document.body.appendChild(source);
+
+    const { container, getByText } = render(
       <ThemeProvider theme={lightTheme}>
-        <Popover />
+        <Popover source={{ current: source }} visible={true}>
+          <div>Hello world</div>
+        </Popover>
       </ThemeProvider>,
+      { container: document.body },
     );
 
-    expect(container.firstChild).toMatchInlineSnapshot(`null`);
+    await waitFor(() => {
+      expect(getByText('Hello world')).toBeInTheDocument();
+    });
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        background: #ffffff;
+        padding: 4px;
+        border-radius: 4px;
+      }
+
+      .c2 {
+        padding-right: 4px;
+      }
+
+      .c1 {
+        box-shadow: 0px 1px 4px rgba(33,33,52,0.1);
+        display: block;
+        position: absolute;
+        border: 1px solid #eaeaef;
+        background: #ffffff;
+      }
+
+      .c3 {
+        max-height: 15rem;
+        overflow-y: scroll;
+      }
+
+      .c3::-webkit-scrollbar {
+        width: 4px;
+      }
+
+      .c3::-webkit-scrollbar-track {
+        background: #ffffff;
+      }
+
+      .c3::-webkit-scrollbar-thumb {
+        background: #eaeaef;
+        border-radius: 4px;
+        margin-right: 10px;
+      }
+
+      <div
+        data-react-portal="true"
+      >
+        <div
+          class="c0 c1"
+        >
+          <div
+            class="c2 c3"
+          >
+            <div>
+              Hello world
+            </div>
+          </div>
+        </div>
+      </div>
+    `);
   });
 });
