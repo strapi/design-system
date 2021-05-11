e28eb2d33dbe23a80e9fb414ed1076ed389fe793
mfrachet <marvin.frachet@strapi.io>
2021-05-11 10:10:28 +0200

    Fix tests

--- packages/strapi-design-system/src/Tooltip/__tests__/Tooltip.spec.js
+++ packages/strapi-design-system/src/Tooltip/__tests__/Tooltip.spec.js
@@ -12,7 +12,7 @@ describe('Tooltip', () => {
   it('snapshots document.body when the tooltip is not shown (aria-describedby does not exist)', () => {
     render(
       <ThemeProvider theme={lightTheme}>
-        <Tooltip content="Content of the tooltip fefe">
+        <Tooltip description="Content of the tooltip fefe">
           <button>Show tooltip</button>
         </Tooltip>
       </ThemeProvider>,
@@ -40,6 +40,7 @@ describe('Tooltip', () => {
       <body>
         <div>
           <button
+            aria-describedby="tooltip-123"
             tabindex="0"
           >
             Show tooltip
@@ -50,13 +51,14 @@ describe('Tooltip', () => {
         >
           <div
             class="c0 c1"
-            content="Content of the tooltip fefe"
             id="tooltip-123"
             role="tooltip"
           >
             <p
               class="c2"
-            />
+            >
+              Content of the tooltip fefe
+            </p>
           </div>
         </div>
       </body>
@@ -66,7 +68,7 @@ describe('Tooltip', () => {
   it('snapshots document.body when the button is focused (aria-describedby exists)', () => {
     render(
       <ThemeProvider theme={lightTheme}>
-        <Tooltip content="Content of the tooltip fefe">
+        <Tooltip description="Content of the tooltip fefe">
           <button>Show tooltip</button>
         </Tooltip>
       </ThemeProvider>,
@@ -78,6 +80,7 @@ describe('Tooltip', () => {
       <body>
         <div>
           <button
+            aria-describedby="tooltip-123"
             tabindex="0"
           >
             Show tooltip
@@ -106,14 +109,74 @@ describe('Tooltip', () => {
         >
           <div
             class="c0 c1"
-            content="Content of the tooltip fefe"
             id="tooltip-123"
             role="tooltip"
             style="left: 0px; top: -8px;"
           >
             <p
               class="c2"
-            />
+            >
+              Content of the tooltip fefe
+            </p>
+          </div>
+        </div>
+      </body>
+    `);
+  });
+
+  it('snapshots document.body with a label', () => {
+    render(
+      <ThemeProvider theme={lightTheme}>
+        <Tooltip label="Content of the tooltip fefe">
+          <button>+</button>
+        </Tooltip>
+      </ThemeProvider>,
+    );
+
+    fireEvent.focus(screen.getByText('+'));
+
+    expect(document.body).toMatchInlineSnapshot(`
+      <body>
+        <div>
+          <button
+            aria-labelledby="tooltip-123"
+            tabindex="0"
+          >
+            +
+          </button>
+        </div>
+        .c0 {
+        background: #212134;
+        padding: 8px;
+        border-radius: 4px;
+      }
+
+      .c2 {
+        font-weight: 500;
+        font-size: 0.75rem;
+        line-height: 1.33;
+        color: #ffffff;
+      }
+
+      .c1 {
+        position: absolute;
+        display: revert;
+      }
+
+      <div
+          data-react-portal="true"
+        >
+          <div
+            class="c0 c1"
+            id="tooltip-123"
+            role="tooltip"
+            style="left: 0px; top: -8px;"
+          >
+            <p
+              class="c2"
+            >
+              Content of the tooltip fefe
+            </p>
           </div>
         </div>
       </body>
