f9d42394697663599a35f048a404f4dda989b83c
mfrachet <marvin.frachet@strapi.io>
2021-05-11 11:40:16 +0200

    Revert

--- packages/strapi-design-system/.storybook/preview.js
+++ packages/strapi-design-system/.storybook/preview.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { ThemeProvider } from '../src/ThemeProvider';
 import { VisuallyHidden } from '../src/VisuallyHidden';
 import { lightTheme } from '../src/themes/light-theme';
-import { Main, SkipToContent } from '../src/Main';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -11,15 +10,14 @@ export const parameters = {
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={lightTheme}>
-      <SkipToContent>Skip to main content</SkipToContent>
-      <Main labelledBy="main">
+      <main>
         <VisuallyHidden>
           {/* Necessary in order to prevent axe core from providing errors on main / heading */}
-          <h1 id="main">This is a storybook story</h1>
+          <h1>Storybook story</h1>
         </VisuallyHidden>
 
         <Story />
-      </Main>
+      </main>
     </ThemeProvider>
   ),
 ];
