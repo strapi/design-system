dec8e91099e12a7e5551c80746d26bcd81f4bb41
HichamELBSI <elabbassih@gmail.com>
2021-05-11 12:13:19 +0200

    Fix specify conf and sync
    Signed-off-by: HichamELBSI <elabbassih@gmail.com>

---
+++ packages/strapi-design-system/src/themes/shadows.js
@@ -0,0 +1,8 @@
+export const shadowTokenObject = {
+  shadow: {
+    filterShadow: '0px 1px 4px rgba(33, 33, 52, 0.1)',
+    focusShadow: '0px 0px 6px rgba(76, 191, 255, 0.75)',
+    popupShadow: '0px 2px 15px rgba(33, 33, 52, 0.1)',
+    tableShadow: '0px 1px 4px rgba(33, 33, 52, 0.1)',
+  },
+};
