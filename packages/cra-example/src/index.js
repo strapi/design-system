b7c3a889b5c1a34418ed45e4547811d7b349f7e8
mfrachet <marvin.frachet@strapi.io>
2021-05-12 15:34:02 +0200

    wip

--- packages/cra-example/src/index.js
+++ packages/cra-example/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider, lightTheme } from "@strapi/design-system";
 import Main from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 const App = () => {
   return (
@@ -18,8 +17,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
