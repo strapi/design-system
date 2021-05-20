aebae230dca5408f336c6ad4c2b4ad83d22ddd86
mfrachet <marvin.frachet@strapi.io>
2021-05-20 09:30:15 +0200

    Storybook running, webpack building on DS, extracting strapi/icons in peer deps

--- packages/strapi-design-system/webpack.config.js
+++ packages/strapi-design-system/webpack.config.js
@@ -63,6 +63,7 @@ module.exports = {
     react: 'react',
     'react-dom': 'react-dom',
     'styled-components': 'styled-components',
+    '@strapi/icons': '@strapi/icons',
   },
   resolve: {
     alias: {
