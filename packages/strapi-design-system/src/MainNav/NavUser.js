60b27c31a9380adad5a65ffe59f3a00b01c17c07
mfrachet <marvin.frachet@strapi.io>
2021-05-28 15:13:47 +0200

    wip

--- packages/strapi-design-system/src/MainNav/NavUser.js
+++ packages/strapi-design-system/src/MainNav/NavUser.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { Avatar } from '../Avatar';
 import { Text } from '../Text';
 import { Row } from '../Row';
 import { Box } from '../Box';
-import styled from 'styled-components';
+import { useMainNav } from './MainNavContext';
+import { VisuallyHidden } from '../VisuallyHidden';
 
 const NavUserBox = styled(Box)`
   text-decoration: none;
 `;
 
 export const NavUser = ({ src, children, ...props }) => {
+  const condensed = useMainNav();
+
   return (
     <NavUserBox paddingTop={3} paddingBottom={3} paddingLeft={2} paddingRight={2} as="a" {...props}>
       <Row as="span">
         <Avatar src={src} alt="" aria-hidden />
-        <Box paddingLeft={2} as="span">
-          <Text as="span" textColor="neutral600">
-            {children}
-          </Text>
-        </Box>
+        {condensed ? (
+          <VisuallyHidden>
+            <span>{children}</span>
+          </VisuallyHidden>
+        ) : (
+          <Box paddingLeft={2} as="span">
+            <Text as="span" textColor="neutral600">
+              {children}
+            </Text>
+          </Box>
+        )}
       </Row>
     </NavUserBox>
   );
