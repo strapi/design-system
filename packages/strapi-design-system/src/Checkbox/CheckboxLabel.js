136c658c6b5df587c7caea8417db2dcadb41cabc
mfrachet <marvin.frachet@strapi.io>
2021-06-21 10:13:05 +0200

    Add description line to checkbox

---
+++ packages/strapi-design-system/src/Checkbox/CheckboxLabel.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Text } from '../Text';
+import { useField } from '../Field';
+
+const TextLabel = styled(Text)`
+  display: flex;
+  align-items: center;
+  * {
+    cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'default')};
+  }
+`;
+
+export const CheckboxLabel = (props) => {
+  const { id, error, hint, name } = useField();
+
+  let ariaDescription;
+
+  if (error) {
+    ariaDescription = `field-error-${id}`;
+  } else if (hint) {
+    ariaDescription = `field-hint-${id}`;
+  }
+
+  const fieldId = `field-${id}`;
+  const hasError = Boolean(error);
+
+  return (
+    <TextLabel
+      as="label"
+      textColor="neutral800"
+      aria-describedby={ariaDescription}
+      aria-invalid={hasError}
+      {...props}
+    />
+  );
+};
--- packages/strapi-design-system/src/Checkbox/__tests__/Checkbox.e2e.js
+++ packages/strapi-design-system/src/Checkbox/__tests__/Checkbox.e2e.js
@@ -1,35 +1,59 @@
 import { injectAxe, checkA11y } from 'axe-playwright';
 
 describe('Checkbox', () => {
-  beforeEach(async () => {
-    // This is the URL of the Storybook Iframe
-    await page.goto('http://localhost:6006/iframe.html?id=design-system-atoms-checkbox--indeterminate&viewMode=story');
-    await injectAxe(page);
+  describe('base', () => {
+    beforeEach(async () => {
+      // This is the URL of the Storybook Iframe
+      await page.goto(
+        'http://localhost:6006/iframe.html?id=design-system-atoms-checkbox--indeterminate&viewMode=story',
+      );
+      await injectAxe(page);
+    });
+
+    it('triggers axe on the document', async () => {
+      await checkA11y(page);
+    });
+
+    it.jestPlaywrightSkip(
+      { browsers: ['webkit'] },
+      'moves to the next element when pressing tab and select when pressing the spacebar',
+      async () => {
+        await page.focus('text="Child 1"');
+        await page.keyboard.press('Tab');
+        await page.keyboard.press(' ');
+
+        const secondBox = await page.$('#field-child2');
+        expect(await secondBox?.isChecked()).toBe(true);
+      },
+    );
+
+    it.jestPlaywrightSkip(
+      { browsers: ['webkit'] },
+      'select the parent element when all child are selected',
+      async () => {
+        await page.focus('text="Child 1"');
+        await page.keyboard.press('Tab');
+        await page.keyboard.press(' ');
+
+        const secondBox = await page.$('#field-parent');
+        expect(await secondBox?.isChecked()).toBe(true);
+      },
+    );
   });
 
-  it('triggers axe on the document', async () => {
-    await checkA11y(page);
+  describe('hint', () => {
+    it('verifies A11y errors on the hint page', async () => {
+      await page.goto('http://localhost:6006/iframe.html?id=design-system-atoms-checkbox--hint&viewMode=story');
+      await injectAxe(page);
+      await checkA11y(page);
+    });
   });
 
-  it.jestPlaywrightSkip(
-    { browsers: ['webkit'] },
-    'moves to the next element when pressing tab and select when pressing the spacebar',
-    async () => {
-      await page.focus('text="Child 1"');
-      await page.keyboard.press('Tab');
-      await page.keyboard.press(' ');
-
-      const secondBox = await page.$('#child2');
-      expect(await secondBox?.isChecked()).toBe(true);
-    },
-  );
-
-  it.jestPlaywrightSkip({ browsers: ['webkit'] }, 'select the parent element when all child are selected', async () => {
-    await page.focus('text="Child 1"');
-    await page.keyboard.press('Tab');
-    await page.keyboard.press(' ');
-
-    const secondBox = await page.$('#parent');
-    expect(await secondBox?.isChecked()).toBe(true);
+  describe('error', () => {
+    it('verifies A11y errors on the error page', async () => {
+      await page.goto('http://localhost:6006/iframe.html?id=design-system-atoms-checkbox--error&viewMode=story');
+      await injectAxe(page);
+      await checkA11y(page);
+    });
   });
 });
