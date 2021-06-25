67b2cb27d18615105d10be6cd9b284538e97af57
soupette <cyril.lpz@gmail.com>
2021-06-25 16:06:13 +0200

    Fix PR feedback
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- packages/strapi-design-system/jest.config.js
+++ packages/strapi-design-system/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testRegex: 'src/.*\\.spec\\.(jsx|js)$',
+  testRegex: ['src/.*\\.spec\\.(jsx|js)$', 'tests/.*\\.spec.js$'],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect', './test-bundler.js'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
