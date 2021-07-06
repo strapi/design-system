b49af5b83d02023c93823688efa6f6493d4d0bd6
Marvin Frachet <marvin.frachet@strapi.io>
2021-07-06 15:43:52 +0200

    Add card asset background (#166)

--- packages/strapi-design-system/src/Card/CardHeader.js
+++ packages/strapi-design-system/src/Card/CardHeader.js
@@ -5,7 +5,6 @@ import { Row } from '../Row';
 const CardHeaderWrapper = styled(Row)`
   position: relative;
   border-bottom: 1px solid ${({ theme }) => theme.colors.neutral150};
-  height: ${88 / 16}rem;
 `;
 
 export const CardHeader = (props) => {
