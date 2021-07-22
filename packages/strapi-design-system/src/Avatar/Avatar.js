adaf2e27778f135021d5fa8178c71ce2e146767b
mfrachet <marvin.frachet@strapi.io>
2021-07-22 09:25:58 +0200

    Add avatar zoom

--- packages/strapi-design-system/src/Avatar/Avatar.js
+++ packages/strapi-design-system/src/Avatar/Avatar.js
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const avatarSize = '26px';
+const avatarSize = 26;
+const previewSize = 64;
 
 const AvatarImg = styled.img`
   border-radius: 50%;
   display: block;
-  height: ${avatarSize};
-  width: ${avatarSize};
 `;
 
-export const Avatar = ({ src, alt }) => {
-  return <AvatarImg src={src} alt={alt} width={avatarSize} height={avatarSize} />;
+const PreviewContainer = styled.img`
+  border-radius: 50%;
+  position: absolute;
+  transform: translate(-${(previewSize - avatarSize) / 2}px, -100%);
+  margin-top: -${({ theme }) => theme.spaces[1]};
+`;
+
+export const Avatar = ({ src, alt, preview }) => {
+  const [previewVisible, setPreviewVisible] = useState(false);
+
+  return (
+    <span>
+      {preview && previewVisible && (
+        <PreviewContainer
+          aria-hidden
+          alt=""
+          width={`${previewSize}px`}
+          height={`${previewSize}px`}
+          src={preview === true ? src : preview}
+        />
+      )}
+      <AvatarImg
+        src={src}
+        alt={alt}
+        width={`${avatarSize}px`}
+        height={`${avatarSize}px`}
+        onMouseEnter={() => setPreviewVisible(true)}
+        onMouseLeave={() => setPreviewVisible(false)}
+      />
+    </span>
+  );
+};
+
+Avatar.defaultProps = {
+  preview: undefined,
 };
 
 Avatar.propTypes = {
   alt: PropTypes.string.isRequired,
+  preview: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
   src: PropTypes.string.isRequired,
 };
--- packages/strapi-design-system/src/Avatar/__tests__/Avatar.spec.js
+++ packages/strapi-design-system/src/Avatar/__tests__/Avatar.spec.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { Avatar } from '../Avatar';
 import { ThemeProvider } from '../../ThemeProvider';
 import { lightTheme } from '../../themes';
@@ -16,17 +16,109 @@ describe('Avatar', () => {
       .c0 {
         border-radius: 50%;
         display: block;
-        height: 26px;
-        width: 26px;
       }
 
-      <img
-        alt="marvin frachet"
-        class="c0"
-        height="26px"
-        src="https://avatars.githubusercontent.com/u/3874873?v=4"
-        width="26px"
-      />
+      <span>
+        <img
+          alt="marvin frachet"
+          class="c0"
+          height="26px"
+          src="https://avatars.githubusercontent.com/u/3874873?v=4"
+          width="26px"
+        />
+      </span>
+    `);
+  });
+
+  it('snapshots the component with preview (boolean)', () => {
+    const { container } = render(
+      <ThemeProvider theme={lightTheme}>
+        <Avatar src="https://avatars.githubusercontent.com/u/3874873?v=4" alt="marvin frachet" preview />
+      </ThemeProvider>,
+    );
+
+    fireEvent.mouseEnter(container.querySelector('img'));
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c1 {
+        border-radius: 50%;
+        display: block;
+      }
+
+      .c0 {
+        border-radius: 50%;
+        position: absolute;
+        -webkit-transform: translate(-19px,-100%);
+        -ms-transform: translate(-19px,-100%);
+        transform: translate(-19px,-100%);
+        margin-top: -4px;
+      }
+
+      <span>
+        <img
+          alt=""
+          aria-hidden="true"
+          class="c0"
+          height="64px"
+          src="https://avatars.githubusercontent.com/u/3874873?v=4"
+          width="64px"
+        />
+        <img
+          alt="marvin frachet"
+          class="c1"
+          height="26px"
+          src="https://avatars.githubusercontent.com/u/3874873?v=4"
+          width="26px"
+        />
+      </span>
+    `);
+  });
+
+  it('snapshots the component with preview (string)', () => {
+    const { container } = render(
+      <ThemeProvider theme={lightTheme}>
+        <Avatar
+          src="https://avatars.githubusercontent.com/u/3874873?v=4"
+          alt="marvin frachet"
+          preview="https://some-unknown-photo/x.png"
+        />
+      </ThemeProvider>,
+    );
+
+    fireEvent.mouseEnter(container.querySelector('img'));
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c1 {
+        border-radius: 50%;
+        display: block;
+      }
+
+      .c0 {
+        border-radius: 50%;
+        position: absolute;
+        -webkit-transform: translate(-19px,-100%);
+        -ms-transform: translate(-19px,-100%);
+        transform: translate(-19px,-100%);
+        margin-top: -4px;
+      }
+
+      <span>
+        <img
+          alt=""
+          aria-hidden="true"
+          class="c0"
+          height="64px"
+          src="https://some-unknown-photo/x.png"
+          width="64px"
+        />
+        <img
+          alt="marvin frachet"
+          class="c1"
+          height="26px"
+          src="https://avatars.githubusercontent.com/u/3874873?v=4"
+          width="26px"
+        />
+      </span>
     `);
   });
 });
