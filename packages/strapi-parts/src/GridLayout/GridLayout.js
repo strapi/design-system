170938da21eca5db2c7ba299aa974026cefd520a
Marvin Frachet <marvin.frachet@strapi.io>
2021-07-29 11:59:31 +0200

    GridLayout and TwoColumnLayout aware of sideNav (#212)

--- packages/strapi-parts/src/GridLayout/GridLayout.js
+++ packages/strapi-parts/src/GridLayout/GridLayout.js
@@ -4,6 +4,10 @@ import { Box } from '../Box';
 import { Row } from '../Row';
 import styled from 'styled-components';
 
+const FlexBox = styled(Box)`
+  flex: 1;
+`;
+
 const BlockActions = styled(Row)`
   & > * + * {
     margin-left: ${({ theme }) => theme.spaces[2]};
@@ -18,34 +22,39 @@ const GridLayoutWrapper = styled.div`
   grid-gap: ${({ theme }) => theme.spaces[4]};
 `;
 
-export const GridLayout = ({ startActions, endActions, header, children }) => {
+export const GridLayout = ({ startActions, sideNav, endActions, header, children }) => {
   return (
-    <Box>
-      {header}
-      <Box paddingLeft={10} paddingRight={10}>
-        {startActions || endActions ? (
-          <Box paddingBottom={4}>
-            <Row justifyContent="space-between">
-              {startActions && <BlockActions>{startActions}</BlockActions>}
-              {endActions && <BlockActions pullRight>{endActions}</BlockActions>}
-            </Row>
-          </Box>
-        ) : null}
+    <Row alignItems="flex-start">
+      {sideNav}
+      <FlexBox>
+        {header}
+        <Box paddingLeft={10} paddingRight={10}>
+          {startActions || endActions ? (
+            <Box paddingBottom={4}>
+              <Row justifyContent="space-between">
+                {startActions && <BlockActions>{startActions}</BlockActions>}
+                {endActions && <BlockActions pullRight>{endActions}</BlockActions>}
+              </Row>
+            </Box>
+          ) : null}
 
-        <GridLayoutWrapper>{children}</GridLayoutWrapper>
-      </Box>
-    </Box>
+          <GridLayoutWrapper>{children}</GridLayoutWrapper>
+        </Box>
+      </FlexBox>
+    </Row>
   );
 };
 
 GridLayout.defaultProps = {
   endActions: undefined,
   startActions: undefined,
+  sideNav: undefined,
 };
 
 GridLayout.propTypes = {
   children: PropTypes.node.isRequired,
   endActions: PropTypes.node,
   header: PropTypes.node.isRequired,
+  sideNav: PropTypes.node,
   startActions: PropTypes.node,
 };
