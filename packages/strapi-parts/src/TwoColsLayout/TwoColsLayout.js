1a060291bc8c2768defd7fb6e6deb964b450cb7f
mfrachet <marvin.frachet@strapi.io>
2021-07-29 11:38:22 +0200

    TwoCols Layout

--- packages/strapi-parts/src/TwoColsLayout/TwoColsLayout.js
+++ packages/strapi-parts/src/TwoColsLayout/TwoColsLayout.js
@@ -5,6 +5,10 @@ import { Row } from '../Row';
 import { Grid, GridItem } from '../Grid';
 import styled from 'styled-components';
 
+const FlexBox = styled(Box)`
+  flex: 1;
+`;
+
 const BlockActions = styled(Row)`
   & > * + * {
     margin-left: ${({ theme }) => theme.spaces[2]};
@@ -13,46 +17,51 @@ const BlockActions = styled(Row)`
   margin-left: ${({ pullRight }) => (pullRight ? 'auto' : undefined)};
 `;
 
-export const TwoColsLayout = ({ startActions, endActions, header, startCol, endCol }) => {
+export const TwoColsLayout = ({ startActions, sideNav, endActions, header, startCol, endCol }) => {
   return (
-    <Box>
-      {header}
-      <Box paddingLeft={10} paddingRight={10}>
-        {startActions || endActions ? (
-          <Box paddingBottom={4}>
-            <Row justifyContent="space-between">
-              {startActions && <BlockActions>{startActions}</BlockActions>}
-              {endActions && <BlockActions pullRight>{endActions}</BlockActions>}
-            </Row>
-          </Box>
-        ) : null}
+    <Row alignItems="flex-start">
+      {sideNav}
+      <FlexBox>
+        {header}
+        <Box paddingLeft={10} paddingRight={10}>
+          {startActions || endActions ? (
+            <Box paddingBottom={4}>
+              <Row justifyContent="space-between">
+                {startActions && <BlockActions>{startActions}</BlockActions>}
+                {endActions && <BlockActions pullRight>{endActions}</BlockActions>}
+              </Row>
+            </Box>
+          ) : null}
 
-        <Grid gap={4}>
-          <GridItem col={9} s={12}>
-            <Box hasRadius background="neutral0" shadow="tableShadow">
-              {startCol}
-            </Box>
-          </GridItem>
-          <GridItem col={3} s={12}>
-            <Box hasRadius background="neutral0" shadow="tableShadow">
-              {endCol}
-            </Box>
-          </GridItem>
-        </Grid>
-      </Box>
-    </Box>
+          <Grid gap={4}>
+            <GridItem col={9} s={12}>
+              <Box hasRadius background="neutral0" shadow="tableShadow">
+                {startCol}
+              </Box>
+            </GridItem>
+            <GridItem col={3} s={12}>
+              <Box hasRadius background="neutral0" shadow="tableShadow">
+                {endCol}
+              </Box>
+            </GridItem>
+          </Grid>
+        </Box>
+      </FlexBox>
+    </Row>
   );
 };
 
 TwoColsLayout.defaultProps = {
   endActions: undefined,
   startActions: undefined,
+  sideNav: undefined,
 };
 
 TwoColsLayout.propTypes = {
   endActions: PropTypes.node,
   endCol: PropTypes.node,
   header: PropTypes.node.isRequired,
+  sideNav: PropTypes.node,
   startActions: PropTypes.node,
   startCol: PropTypes.node.isRequired,
 };
