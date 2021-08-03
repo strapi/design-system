5e6cf97d458f9c21859920071e173a63abe060a7
ronronscelestes <julie.plantey@gmail.com>
2021-08-03 10:00:36 +0200

    created IconButtonGroup wrapper

--- packages/strapi-parts/src/IconButton/IconButton.js
+++ packages/strapi-parts/src/IconButton/IconButton.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Tooltip } from '../Tooltip';
 import { BaseButton } from '../BaseButton';
+import { Row } from '../Row';
 
 const IconButtonWrapper = styled(BaseButton)`
   svg {
@@ -28,6 +29,49 @@ const IconButtonWrapper = styled(BaseButton)`
     }
   }
   ${({ noBorder }) => (noBorder ? `border: none;` : undefined)}
+`;
+
+export const IconButtonGroup = styled(Row)`
+  & span:first-child button {
+    border-left: 1px solid ${({ theme }) => theme.colors.neutral200};
+    border-radius: ${({ theme }) => `${theme.borderRadius} 0 0 ${theme.borderRadius}`};
+  }
+
+  & span:last-child button {
+    border-radius: ${({ theme }) => `0 ${theme.borderRadius} ${theme.borderRadius} 0`};
+  }
+
+  & ${IconButtonWrapper} {
+    border-radius: 0;
+    border-left: none;
+
+    svg {
+      > g,
+      path {
+        fill: ${({ theme }) => theme.colors.neutral800};
+      }
+    }
+
+    &:hover {
+      background-color: ${({ theme }) => theme.colors.neutral150};
+
+      svg {
+        > g,
+        path {
+          fill: ${({ theme }) => theme.colors.neutral900};
+        }
+      }
+    }
+
+    &:active {
+      svg {
+        > g,
+        path {
+          fill: ${({ theme }) => theme.colors.neutral700};
+        }
+      }
+    }
+  }
 `;
 
 export const IconButton = React.forwardRef(({ label, noBorder, icon, ...props }, ref) => {
