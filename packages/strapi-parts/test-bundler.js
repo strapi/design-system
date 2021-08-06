8b5da09439e1b324b86949116510c418af1aaa66
Marvin Frachet <marvin.frachet@strapi.io>
2021-08-06 11:52:32 +0200

    Attempt to fix Intl based date (#231)

--- packages/strapi-parts/test-bundler.js
+++ packages/strapi-parts/test-bundler.js
@@ -1,4 +1,13 @@
+import IntlPolyfill from 'intl';
+import 'intl/locale-data/jsonp/en';
 import 'jest-styled-components';
+
+if (global.Intl) {
+  Intl.NumberFormat = IntlPolyfill.NumberFormat;
+  Intl.DateTimeFormat = IntlPolyfill.DateTimeFormat;
+} else {
+  global.Intl = IntlPolyfill;
+}
 
 window.ResizeObserver = () => ({
   observe: () => {},
