8b9693c481c9d754aa21ea83705a333e438b3d98
mfrachet <marvin.frachet@strapi.io>
2021-08-13 09:07:30 +0200

    Table stories with axe

--- packages/strapi-parts/src/Table/__tests__/Table.e2e.js
+++ packages/strapi-parts/src/Table/__tests__/Table.e2e.js
@@ -1,13 +1,21 @@
 import { injectAxe, checkA11y } from 'axe-playwright';
 
 describe('Table', () => {
-  beforeEach(async () => {
-    // This is the URL of the Storybook Iframe
-    await page.goto('http://localhost:6006/iframe.html?id=design-system-organisms-table--base&viewMode=story');
-    await injectAxe(page);
+  describe('base', () => {
+    it('triggers axe on the document', async () => {
+      await page.goto('http://localhost:6006/iframe.html?id=design-system-organisms-table--base&viewMode=story');
+      await injectAxe(page);
+      await checkA11y(page);
+    });
   });
 
-  it('triggers axe on the document', async () => {
-    await checkA11y(page);
+  describe('with actions', () => {
+    it('triggers axe on the document', async () => {
+      await page.goto(
+        'http://localhost:6006/iframe.html?id=design-system-organisms-table--with-th-actions&viewMode=story',
+      );
+      await injectAxe(page);
+      await checkA11y(page);
+    });
   });
 });
