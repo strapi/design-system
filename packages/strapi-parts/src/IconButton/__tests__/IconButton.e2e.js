ed29634d292057b9e740ad18843ecf911c915e7a
mfrachet <marvin.frachet@strapi.io>
2021-08-13 07:57:49 +0200

    IconButton E2E tests

--- packages/strapi-parts/src/IconButton/__tests__/IconButton.e2e.js
+++ packages/strapi-parts/src/IconButton/__tests__/IconButton.e2e.js
@@ -1,13 +1,29 @@
 import { injectAxe, checkA11y } from 'axe-playwright';
 
 describe('IconButton', () => {
-  beforeEach(async () => {
-    // This is the URL of the Storybook Iframe
-    await page.goto('http://localhost:6006/iframe.html?id=design-system-molecules-iconbutton--base&viewMode=story');
-    await injectAxe(page);
+  describe('base', () => {
+    it('triggers axe on the document', async () => {
+      await page.goto('http://localhost:6006/iframe.html?id=design-system-molecules-iconbutton--base&viewMode=story');
+      await injectAxe(page);
+      await checkA11y(page);
+    });
   });
 
-  it('triggers axe on the document', async () => {
-    await checkA11y(page);
+  describe('disabled', () => {
+    it('triggers axe on the document', async () => {
+      await page.goto(
+        'http://localhost:6006/iframe.html?id=design-system-molecules-iconbutton--disabled&viewMode=story',
+      );
+      await injectAxe(page);
+      await checkA11y(page);
+    });
+  });
+
+  describe('group', () => {
+    it('triggers axe on the document', async () => {
+      await page.goto('http://localhost:6006/iframe.html?id=design-system-molecules-iconbutton--group&viewMode=story');
+      await injectAxe(page);
+      await checkA11y(page);
+    });
   });
 });
