90dc6be857eea1e45f34b0900f4d48f7e6153a98
mfrachet <marvin.frachet@strapi.io>
2021-08-13 08:56:18 +0200

    Searchbar disabled tests

--- packages/strapi-parts/src/Searchbar/__tests__/Searchbar.e2e.js
+++ packages/strapi-parts/src/Searchbar/__tests__/Searchbar.e2e.js
@@ -1,21 +1,29 @@
 import { injectAxe, checkA11y } from 'axe-playwright';
 
 describe('Searchbar', () => {
-  beforeEach(async () => {
-    // This is the URL of the Storybook Iframe
-    await page.goto('http://localhost:6006/iframe.html?id=design-system-molecules-searchbar--base&viewMode=story');
-    await injectAxe(page);
+  describe('base', () => {
+    it('triggers axe on the document', async () => {
+      await page.goto('http://localhost:6006/iframe.html?id=design-system-molecules-searchbar--base&viewMode=story');
+      await injectAxe(page);
+      await checkA11y(page);
+    });
+
+    it('brings back the focus to the input when clearing it', async () => {
+      await page.fill('input', 'Hello world');
+      expect(await page.$eval('input', (el) => el.value)).toBe('Hello world');
+
+      await page.click('[aria-label="Clearing the plugin search"]');
+      expect(await page.$eval('input', (el) => el.value)).toBe('');
+    });
   });
 
-  it('triggers axe on the document', async () => {
-    await checkA11y(page);
-  });
-
-  it('brings back the focus to the input when clearing it', async () => {
-    await page.fill('input', 'Hello world');
-    expect(await page.$eval('input', (el) => el.value)).toBe('Hello world');
-
-    await page.click('[aria-label="Clearing the plugin search"]');
-    expect(await page.$eval('input', (el) => el.value)).toBe('');
+  describe('disabled', () => {
+    it('triggers axe on the document', async () => {
+      await page.goto(
+        'http://localhost:6006/iframe.html?id=design-system-molecules-searchbar--disabled&viewMode=story',
+      );
+      await injectAxe(page);
+      await checkA11y(page);
+    });
   });
 });
