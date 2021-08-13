93ece38084efebbe7ceeff2e521087435406201d
mfrachet <marvin.frachet@strapi.io>
2021-08-13 09:00:49 +0200

    axe shoot on simple menu with links

--- packages/strapi-parts/src/SimpleMenu/__tests__/SimpleMenu.e2e.js
+++ packages/strapi-parts/src/SimpleMenu/__tests__/SimpleMenu.e2e.js
@@ -1,40 +1,73 @@
 import { injectAxe, checkA11y, getViolations } from 'axe-playwright';
 
 describe('SimpleMenu', () => {
-  beforeEach(async () => {
-    // This is the URL of the Storybook Iframe
-    await page.goto('http://localhost:6006/iframe.html?id=design-system-molecules-simplemenu--base&viewMode=story');
-    await injectAxe(page);
+  describe('base', () => {
+    beforeEach(async () => {
+      // This is the URL of the Storybook Iframe
+      await page.goto('http://localhost:6006/iframe.html?id=design-system-molecules-simplemenu--base&viewMode=story');
+      await injectAxe(page);
+    });
+
+    it('triggers axe on the document', async () => {
+      await checkA11y(page);
+    });
+
+    it('triggers axe on the document when the menu is opened', async () => {
+      await page.click('button');
+
+      await expect(page).toHaveSelector('[role="menu"]');
+
+      const violations = await getViolations(page);
+
+      // Axe throws an error because the portal is not wrapped by a region.
+      const realViolations = violations
+        .filter((violation) => violation.id !== 'region')
+        .filter((violation) => violation.id !== 'aria-required-parent');
+
+      console.log('realViolations', realViolations);
+
+      expect(realViolations.length).toBe(0);
+    });
+
+    it.each(['Enter', 'Space'])('select the second value of the menu', async (keyPressed) => {
+      await page.focus('button');
+      await page.keyboard.press(keyPressed);
+      await page.keyboard.press('ArrowDown');
+      await page.keyboard.press(keyPressed);
+
+      const label = await page.textContent('button');
+      expect(label).toBe('February');
+    });
   });
 
-  it('triggers axe on the document', async () => {
-    await checkA11y(page);
-  });
+  describe('with links', () => {
+    beforeEach(async () => {
+      // This is the URL of the Storybook Iframe
+      await page.goto(
+        'http://localhost:6006/iframe.html?id=design-system-molecules-simplemenu--with-links&viewMode=story',
+      );
+      await injectAxe(page);
+    });
 
-  it('triggers axe on the document when the menu is opened', async () => {
-    await page.click('button');
+    it('triggers axe on the document', async () => {
+      await checkA11y(page);
+    });
 
-    await expect(page).toHaveSelector('[role="menu"]');
+    it('triggers axe on the document when the menu is opened', async () => {
+      await page.click('button');
 
-    const violations = await getViolations(page);
+      await expect(page).toHaveSelector('[role="menu"]');
 
-    // Axe throws an error because the portal is not wrapped by a region.
-    const realViolations = violations
-      .filter((violation) => violation.id !== 'region')
-      .filter((violation) => violation.id !== 'aria-required-parent');
+      const violations = await getViolations(page);
 
-    console.log('realViolations', realViolations);
+      // Axe throws an error because the portal is not wrapped by a region.
+      const realViolations = violations
+        .filter((violation) => violation.id !== 'region')
+        .filter((violation) => violation.id !== 'aria-required-parent');
 
-    expect(realViolations.length).toBe(0);
-  });
+      console.log('realViolations', realViolations);
 
-  it.each(['Enter', 'Space'])('select the second value of the menu', async (keyPressed) => {
-    await page.focus('button');
-    await page.keyboard.press(keyPressed);
-    await page.keyboard.press('ArrowDown');
-    await page.keyboard.press(keyPressed);
-
-    const label = await page.textContent('button');
-    expect(label).toBe('February');
+      expect(realViolations.length).toBe(0);
+    });
   });
 });
