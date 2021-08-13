4d9cb81ae8d69e79449e72266a9619bdebe40129
mfrachet <marvin.frachet@strapi.io>
2021-08-13 09:13:50 +0200

    Alert stories and tests

--- packages/strapi-parts/src/Alert/__tests__/Alert.e2e.js
+++ packages/strapi-parts/src/Alert/__tests__/Alert.e2e.js
@@ -1,13 +1,27 @@
 import { injectAxe, checkA11y } from 'axe-playwright';
 
 describe('Alert', () => {
-  beforeEach(async () => {
-    // This is the URL of the Storybook Iframe
-    await page.goto('http://localhost:6006/iframe.html?id=design-system-molecules-alert--default&viewMode=story');
-    await injectAxe(page);
+  describe('base', () => {
+    it('triggers axe on the document', async () => {
+      await page.goto('http://localhost:6006/iframe.html?id=design-system-molecules-alert--base&viewMode=story');
+      await injectAxe(page);
+      await checkA11y(page);
+    });
   });
 
-  it('triggers axe on the document', async () => {
-    await checkA11y(page);
+  describe('variants', () => {
+    it('triggers axe on the document', async () => {
+      await page.goto('http://localhost:6006/iframe.html?id=design-system-molecules-alert--variants&viewMode=story');
+      await injectAxe(page);
+      await checkA11y(page);
+    });
+  });
+
+  describe('with action', () => {
+    it('triggers axe on the document', async () => {
+      await page.goto('http://localhost:6006/iframe.html?id=design-system-molecules-alert--with-action&viewMode=story');
+      await injectAxe(page);
+      await checkA11y(page);
+    });
   });
 });
