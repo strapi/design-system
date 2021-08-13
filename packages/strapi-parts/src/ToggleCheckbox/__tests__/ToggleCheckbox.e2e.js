6adfba4b21e3dfdad037dc1e015da8858d81b664
mfrachet <marvin.frachet@strapi.io>
2021-08-13 09:11:10 +0200

    Improving stories and add tests

--- packages/strapi-parts/src/ToggleCheckbox/__tests__/ToggleCheckbox.e2e.js
+++ packages/strapi-parts/src/ToggleCheckbox/__tests__/ToggleCheckbox.e2e.js
@@ -1,13 +1,23 @@
 import { injectAxe, checkA11y } from 'axe-playwright';
 
 describe('ToggleCheckbox', () => {
-  beforeEach(async () => {
-    // This is the URL of the Storybook Iframe
-    await page.goto('http://localhost:6006/iframe.html?id=design-system-molecules-togglecheckbox--base&viewMode=story');
-    await injectAxe(page);
+  describe('activated', () => {
+    it('triggers axe on the document', async () => {
+      await page.goto(
+        'http://localhost:6006/iframe.html?id=design-system-molecules-togglecheckbox--activated&viewMode=storyy',
+      );
+      await injectAxe(page);
+      await checkA11y(page);
+    });
   });
 
-  it('triggers axe on the document', async () => {
-    await checkA11y(page);
+  describe('not activated', () => {
+    it('triggers axe on the document', async () => {
+      await page.goto(
+        'http://localhost:6006/iframe.html?id=design-system-molecules-togglecheckbox--not-activated&viewMode=story',
+      );
+      await injectAxe(page);
+      await checkA11y(page);
+    });
   });
 });
