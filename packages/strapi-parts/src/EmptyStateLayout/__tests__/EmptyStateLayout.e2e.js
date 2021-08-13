5670ce8a2639a68ff037bac785b04e314570be4a
mfrachet <marvin.frachet@strapi.io>
2021-08-13 07:46:59 +0200

    EmptyState layout

--- packages/strapi-parts/src/EmptyStateLayout/__tests__/EmptyStateLayout.e2e.js
+++ packages/strapi-parts/src/EmptyStateLayout/__tests__/EmptyStateLayout.e2e.js
@@ -1,13 +1,23 @@
 import { injectAxe, checkA11y } from 'axe-playwright';
 
 describe('EmptyStateLayout', () => {
-  beforeEach(async () => {
-    // This is the URL of the Storybook Iframe
-    await page.goto('http://localhost:6006/iframe.html?id=design-system-layouts-emptystatelayout--base&viewMode=story');
-    await injectAxe(page);
+  describe('base', () => {
+    it('triggers axe on the document', async () => {
+      await page.goto(
+        'http://localhost:6006/iframe.html?id=design-system-layouts-emptystatelayout--base&viewMode=story',
+      );
+      await injectAxe(page);
+      await checkA11y(page);
+    });
   });
 
-  it('triggers axe on the document', async () => {
-    await checkA11y(page);
+  describe('without action', () => {
+    it('triggers axe on the document', async () => {
+      await page.goto(
+        'http://localhost:6006/iframe.html?id=design-system-layouts-emptystatelayout--without-action&viewMode=story',
+      );
+      await injectAxe(page);
+      await checkA11y(page);
+    });
   });
 });
