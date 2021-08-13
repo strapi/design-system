66623d020d7369ab6091bc81e7cef9b5bcee2bf3
mfrachet <marvin.frachet@strapi.io>
2021-08-13 09:03:02 +0200

    Tests on Stack

--- packages/strapi-parts/src/Stack/__tests__/Stack.e2e.js
+++ packages/strapi-parts/src/Stack/__tests__/Stack.e2e.js
@@ -1,13 +1,19 @@
 import { injectAxe, checkA11y } from 'axe-playwright';
 
 describe('Stack', () => {
-  beforeEach(async () => {
-    // This is the URL of the Storybook Iframe
-    await page.goto('http://localhost:6006/iframe.html?id=design-system-atoms-stack--base&viewMode=story');
-    await injectAxe(page);
+  describe('vertical', () => {
+    it('triggers axe on the document', async () => {
+      await page.goto('http://localhost:6006/iframe.html?id=design-system-atoms-stack--base&viewMode=story');
+      await injectAxe(page);
+      await checkA11y(page);
+    });
   });
 
-  it('triggers axe on the document', async () => {
-    await checkA11y(page);
+  describe('horizontal', () => {
+    it('triggers axe on the document', async () => {
+      await page.goto('http://localhost:6006/iframe.html?id=design-system-atoms-stack--horizontal&viewMode=story');
+      await injectAxe(page);
+      await checkA11y(page);
+    });
   });
 });
