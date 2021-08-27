4a263104a455f0ee9ea055fd0fb1c95387eebfe3
ronronscelestes <julie.plantey@gmail.com>
2021-08-27 11:53:14 +0200

    removed a log + updated tests

--- packages/strapi-parts/src/Dialog/DialogBody.js
+++ packages/strapi-parts/src/Dialog/DialogBody.js
@@ -16,7 +16,6 @@ const IconContainer = styled(Box)`
 `;
 
 export const DialogBody = ({ children, icon }) => {
-  console.log(children)
   return (
     <Box paddingTop={8} paddingBottom={8} paddingLeft={6} paddingRight={6}>
       {icon && (
