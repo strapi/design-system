cb62df17b1dfa977c4cd7377d9333ce3d4675f53
ronronscelestes <julie.plantey@gmail.com>
2021-08-30 12:22:01 +0200

    placed component in DS/molecules + updated tests

--- packages/strapi-parts/src/ToggleInput/__tests__/ToggleInput.e2e.js
+++ packages/strapi-parts/src/ToggleInput/__tests__/ToggleInput.e2e.js
@@ -3,7 +3,9 @@ import { injectAxe, checkA11y } from 'axe-playwright';
 describe('ToggleInput', () => {
   describe('base', () => {
     it('triggers axe on the document', async () => {
-      await page.goto('http://localhost:6006/iframe.html?id=toggleinput--base&args=&viewMode=story');
+      await page.goto(
+        'http://localhost:6006/iframe.html?id=design-system-molecules-toggleinput--base&args=&viewMode=story',
+      );
       await injectAxe(page);
       await checkA11y(page);
     });
@@ -11,7 +13,9 @@ describe('ToggleInput', () => {
 
   describe('input error', () => {
     it('triggers axe on the document', async () => {
-      await page.goto('http://localhost:6006/iframe.html?id=toggleinput--error&args=&viewMode=story');
+      await page.goto(
+        'http://localhost:6006/iframe.html?id=design-system-molecules-toggleinput--error&args=&viewMode=story',
+      );
       await injectAxe(page);
       await checkA11y(page);
     });
