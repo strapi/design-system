263896f824854994779da7c8adb7d0dbbae30eeb
bulby97 <s.montlouis.calixte@gmail.com>
2021-08-30 09:36:50 +0200

    Fix aria label for optgroup

--- packages/strapi-parts/src/Select/OptGroup.js
+++ packages/strapi-parts/src/Select/OptGroup.js
@@ -6,7 +6,12 @@ export const OptGroup = ({ children, label, ...props }) => {
   const childrenValues = children.map((child) => child.props.value);
   return (
     <>
-      <Option data-opt-group={true} data-opt-group-children={childrenValues} {...props}>
+      <Option
+        data-opt-group={true}
+        data-opt-group-children={childrenValues}
+        aria-label={`${label}, ${children.length} items: ${childrenValues.join(', ')}`}
+        {...props}
+      >
         {label}
       </Option>
       {children}
