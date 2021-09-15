104740c044a780c9a2179cf8127ce3abe71330f5
mfrachet <marvin.frachet@strapi.io>
2021-09-15 11:07:15 +0200

    Allowing IconButton to not receive a label

--- packages/strapi-parts/src/ModalLayout/ModalHeader.js
+++ packages/strapi-parts/src/ModalLayout/ModalHeader.js
@@ -18,7 +18,7 @@ export const ModalHeader = ({ children, closeLabel }) => {
     <ModalHeaderWrapper paddingTop={4} paddingBottom={4} paddingLeft={5} paddingRight={5} background="neutral100">
       <Row justifyContent="space-between">
         {children}
-        <IconButton onClick={onClose} label={closeLabel} icon={<CloseAlertIcon />} />
+        <IconButton onClick={onClose} aria-label={closeLabel} icon={<CloseAlertIcon />} />
       </Row>
     </ModalHeaderWrapper>
   );
