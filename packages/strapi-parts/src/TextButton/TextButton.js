0cb30f52de80746be5903f829138108135cbeb43
mfrachet <marvin.frachet@strapi.io>
2021-10-04 17:37:42 +0200

    Wip

--- packages/strapi-parts/src/TextButton/TextButton.js
+++ packages/strapi-parts/src/TextButton/TextButton.js
@@ -8,7 +8,13 @@ import { Row } from '../Row';
 const TextButtonWrapper = styled(Row)`
   background: transparent;
   border: none;
-  pointer-events: ${({ disabled }) => (disabled ? 'none' : undefined)};
+
+  &[aria-disabled='true'] {
+    pointer-events: none;
+    svg path {
+      fill: ${({ theme }) => theme.colors.neutral600};
+    }
+  }
 
   svg {
     display: flex;
@@ -16,7 +22,7 @@ const TextButtonWrapper = styled(Row)`
   }
 
   svg path {
-    fill: ${({ disabled, theme }) => (disabled ? theme.colors.neutral600 : theme.colors.primary600)};
+    fill: ${({ theme }) => theme.colors.primary600};
   }
 `;
 
@@ -24,15 +30,7 @@ export const TextButton = React.forwardRef(({ children, startIcon, endIcon, onCl
   const handleClick = onClick && !disabled ? onClick : undefined;
 
   return (
-    <TextButtonWrapper
-      ref={ref}
-      aria-disabled={disabled}
-      disabled={disabled}
-      onClick={handleClick}
-      as="button"
-      type="button"
-      {...props}
-    >
+    <TextButtonWrapper ref={ref} aria-disabled={disabled} onClick={handleClick} as="button" type="button" {...props}>
       {startIcon && (
         <Box as="span" paddingRight={2} aria-hidden={true}>
           {startIcon}
