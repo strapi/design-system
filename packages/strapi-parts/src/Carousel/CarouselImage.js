3e7148d162c447fc7abeddaf76c0933014a22f30
Marvin Frachet <marvin.frachet@strapi.io>
2021-10-25 11:42:55 +0200

    Fix carousel type button and next/back button visibility (#394)

--- packages/strapi-parts/src/Carousel/CarouselImage.js
+++ packages/strapi-parts/src/Carousel/CarouselImage.js
@@ -1,5 +1,4 @@
 import React from 'react';
+import { Box } from '../Box';
 
-export const CarouselImage = (props) => {
-  return <img height="124px" {...props} />;
-};
+export const CarouselImage = (props) => <Box as="img" height="100%" {...props} />;
