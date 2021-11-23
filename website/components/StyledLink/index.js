b8e83fa0c0d462f4bbd4ecbe4eb0463ff4ac16e5
Bassel Kanso <basselkanso82@gmail.com>
2021-11-23 19:01:07 +0300

    use {} to spread props object and pass theme

--- website/components/StyledLink/index.js
+++ website/components/StyledLink/index.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
 const StyledLink = styled.a`
-  color: ${(props) => (props.active ? props.theme.colors.primary600 : props.theme.colors.neutral600)};
-  font-size: ${(props) => props.theme.fontSizes[props.size]};
+  color: ${({ theme, active }) => (active ? theme.colors.primary600 : theme.colors.neutral600)};
+  font-size: ${({ theme, size }) => theme.fontSizes[size]};
   display: flex;
   align-content: center;
-  padding: ${(props) => `${props.theme.spaces[props.padding]} 0`};
+  padding: ${({ theme, padding }) => `${theme.spaces[padding]} 0`};
   text-decoration: none;
 `;
 
