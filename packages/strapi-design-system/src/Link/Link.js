9e16ca61e1903d5feea0e03568b906c16ed085a2
ronronscelestes <julie.plantey@gmail.com>
2021-12-22 11:18:27 +0100

    Link based on BaseLink

--- packages/strapi-design-system/src/Link/Link.js
+++ packages/strapi-design-system/src/Link/Link.js
@@ -1,46 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import ExternalLink from '@strapi/icons/ExternalLink';
 import { NavLink } from 'react-router-dom';
-import { Typography } from '../Typography';
-import { Box } from '../Box';
-import { buttonFocusStyle } from '../themes/utils';
-
-const LinkWrapper = styled.a`
-  display: inline-flex;
-  align-items: center;
-  text-transform: uppercase;
-  text-decoration: none;
-  pointer-events: ${({ disabled }) => (disabled ? 'none' : undefined)};
-
-  svg path {
-    fill: ${({ disabled, theme }) => (disabled ? theme.colors.neutral600 : theme.colors.primary600)};
-  }
-
-  svg {
-    font-size: ${10 / 16}rem;
-  }
-
-  &:hover,
-  &:active {
-    color: ${({ theme }) => theme.colors.primary800};
-  }
-
-  ${buttonFocusStyle};
-`;
-
-const IconWrapper = styled(Box)`
-  display: flex;
-`;
+import { BaseLink } from '../BaseLink';
 
 // TODO: make sure to use the link from the router library chosen
-export const Link = ({ href, to, children, disabled, startIcon, endIcon, ...props }) => {
+export const Link = ({ href, to, disabled, ...props }) => {
   const target = href ? '_blank' : undefined;
   const rel = href ? 'noreferrer noopener' : undefined;
 
   return (
-    <LinkWrapper
+    <BaseLink
       as={to && !disabled ? NavLink : 'a'}
       target={target}
       rel={rel}
@@ -48,29 +17,7 @@ export const Link = ({ href, to, children, disabled, startIcon, endIcon, ...prop
       href={disabled ? '#' : href}
       disabled={disabled}
       {...props}
-    >
-      {startIcon && (
-        <IconWrapper as="span" aria-hidden={true} paddingRight={2}>
-          {startIcon}
-        </IconWrapper>
-      )}
-
-      <Typography variant="sigma" textColor={disabled ? 'neutral600' : 'primary600'}>
-        {children}
-      </Typography>
-
-      {endIcon && !href && (
-        <IconWrapper as="span" aria-hidden={true} paddingLeft={2}>
-          {endIcon}
-        </IconWrapper>
-      )}
-
-      {href && (
-        <IconWrapper as="span" aria-hidden={true} paddingLeft={2}>
-          <ExternalLink />
-        </IconWrapper>
-      )}
-    </LinkWrapper>
+    />
   );
 };
 
@@ -83,15 +30,12 @@ Link.defaultProps = {
 };
 
 Link.propTypes = {
-  children: PropTypes.node.isRequired,
   disabled: PropTypes.bool,
-  endIcon: PropTypes.element,
   href: (props) => {
     if (!props.disabled && !props.to && !props.href) {
       return new Error('href must be defined');
     }
   },
-  startIcon: PropTypes.element,
   to: (props) => {
     if (!props.disabled && !props.href && !props.to) {
       return new Error('to must be defined');
