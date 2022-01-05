888c8a992669e425a005137f2d763d3035867b6c
Bassel Kanso <basselkanso82@gmail.com>
2022-01-05 11:04:55 +0300

    add analytics stuff

--- website/pages/_document.js
+++ website/pages/_document.js
@@ -9,8 +9,23 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            type="text/plain"
+            data-cookieconsent="statistics"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-KN9JRWG');`,
+            }}
+          ></script>
+        </Head>
         <body>
+          <iframe
+            title="google tag manager"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KN9JRWG"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          />
           <Main />
           <NextScript />
         </body>
