e3ae3bed9054afea6c0d5fff474d67349f52fd5d
HichamELBSI <elabbassih@gmail.com>
2022-01-12 16:06:15 +0100

    Fix snapshotst
    Signed-off-by: HichamELBSI <elabbassih@gmail.com>

--- packages/strapi-design-system/src/Loader/Loader.js
+++ packages/strapi-design-system/src/Loader/Loader.js
@@ -2,7 +2,7 @@ import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import styled, { keyframes } from 'styled-components';
 import { VisuallyHidden } from '../VisuallyHidden';
-import loaderSvg from './assets/loader.svg';
+import LoaderSvg from './LoaderSvg';
 
 const rotation = keyframes`
   from {
@@ -13,16 +13,19 @@ const rotation = keyframes`
   }
 `;
 
-const LoaderImg = styled.img`
+const LoaderImg = styled(LoaderSvg)`
   animation: ${rotation} 1s infinite linear;
   ${({ small }) => small && `width: 25px; height: 25px;`}
+  path {
+    fill: ${({ color, theme }) => theme.colors[color]};
+  }
 `;
 
-export const Loader = forwardRef(({ children, small, ...props }, ref) => {
+export const Loader = forwardRef(({ children, small, color, ...props }, ref) => {
   return (
     <div role="alert" aria-live="assertive" ref={ref} {...props}>
       <VisuallyHidden>{children}</VisuallyHidden>
-      <LoaderImg src={loaderSvg} aria-hidden={true} small={small} />
+      <LoaderImg small={small} color={color} />
     </div>
   );
 });
@@ -31,9 +34,11 @@ Loader.displayName = 'Loader';
 
 Loader.defaultProps = {
   small: false,
+  color: 'primary600',
 };
 
 Loader.propTypes = {
   children: PropTypes.string.isRequired,
+  color: PropTypes.string,
   small: PropTypes.bool,
 };
