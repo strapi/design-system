86ee3d87e79027f4e5d61703eb0c1d15ba101ee0
ronronscelestes <julie.plantey@gmail.com>
2022-01-27 22:40:57 +0100

    updated branch and updated CarouselInput e2e tests

--- packages/strapi-design-system/src/CarouselInput/__tests__/CarouselInput.e2e.js
+++ packages/strapi-design-system/src/CarouselInput/__tests__/CarouselInput.e2e.js
@@ -1,9 +1,9 @@
 import { injectAxe, checkA11y } from 'axe-playwright';
 
-describe('Carousel', () => {
+describe('CarouselInput', () => {
   beforeEach(async () => {
     // This is the URL of the Storybook Iframe
-    await page.goto('http://localhost:6006/iframe.html?id=design-system-components-carousel--base&viewMode=story');
+    await page.goto('http://localhost:6006/iframe.html?id=design-system-components-carouselinput--base&viewMode=story');
     await injectAxe(page);
   });
 
