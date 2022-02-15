7545fbaf5adaa7061da54fe2c5cddf79dcd7248e
Bassel Kanso <basselkanso82@gmail.com>
2022-02-15 10:09:08 +0300

    add google tag manager

--- website/pages/_document.js
+++ website/pages/_document.js
@@ -10,7 +10,30 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
+        <script
+          id="Cookiebot"
+          src="https://consent.cookiebot.com/uc.js"
+          data-cbid="609a66e2-0531-441d-b473-6cbd335dd21e"
+          data-blockingmode="auto"
+          type="text/javascript"
+        />
+        <script
+          type="text/plain"
+          data-cookieconsent="statistics"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-KN9JRWG');`,
+          }}
+        ></script>
         <body>
+          <iframe
+            title="google tag manager"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KN9JRWG"
+            height="0"
+            width="0"
+            style={{
+              display: 'none',
+            }}
+          />
           <Main />
           <NextScript />
         </body>
