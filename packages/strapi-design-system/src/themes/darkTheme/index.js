bd3bfe1ba36272bede6dd73c84e1061c9534d49b
ronronscelestes <julie.plantey@gmail.com>
2022-02-18 15:47:16 +0100

    fixed position of colors in lightTheme and darkTheme

--- packages/strapi-design-system/src/themes/darkTheme/index.js
+++ packages/strapi-design-system/src/themes/darkTheme/index.js
@@ -2,6 +2,6 @@ import { darkColorTokenObject } from './dark-colors';
 import { commonTheme } from '../common-theme';
 
 export const darkTheme = {
-  ...commonTheme,
   colors: darkColorTokenObject.color,
+  ...commonTheme,
 };
--- packages/strapi-design-system/src/themes/lightTheme/index.js
+++ packages/strapi-design-system/src/themes/lightTheme/index.js
@@ -2,6 +2,6 @@ import { lightColorTokenObject } from './light-colors';
 import { commonTheme } from '../common-theme';
 
 export const lightTheme = {
-  ...commonTheme,
   colors: lightColorTokenObject.color,
+  ...commonTheme,
 };
