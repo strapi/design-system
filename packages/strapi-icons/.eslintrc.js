210618d045863b9e8203ebe443d1cb7ffbf9bddb
HichamELBSI <elabbassih@gmail.com>
2022-02-23 13:57:17 +0100

    Fix icons build
    Signed-off-by: HichamELBSI <elabbassih@gmail.com>

---
+++ packages/strapi-icons/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+};
