8f6df8080e8fca5d0e535eea2d91c9819b88d389
Gustav Hansen <gustav.hansen@strapi.io>
2022-03-01 13:51:30 +0100

    ToggleCheckbox: cleanup tests

--- packages/strapi-design-system/src/ToggleCheckbox/__tests__/ToggleCheckbox.e2e.js
+++ packages/strapi-design-system/src/ToggleCheckbox/__tests__/ToggleCheckbox.e2e.js
@@ -3,19 +3,9 @@ const { injectAxe, checkA11y } = require('axe-playwright');
 const { test } = require('@playwright/test');
 
 test.describe.parallel('ToggleCheckbox', () => {
-  test.describe('activated', () => {
-    test('triggers axe on the document', async ({ page }) => {
-      await page.goto('/iframe.html?id=design-system-components-togglecheckbox--activated&viewMode=storyy');
-      await injectAxe(page);
-      await checkA11y(page);
-    });
-  });
-
-  test.describe('not activated', () => {
-    test('triggers axe on the document', async ({ page }) => {
-      await page.goto('/iframe.html?id=design-system-components-togglecheckbox--not-activated&viewMode=story');
-      await injectAxe(page);
-      await checkA11y(page);
-    });
+  test('triggers axe on the document', async ({ page }) => {
+    await page.goto('/iframe.html?id=design-system-components-togglecheckbox--base&viewMode=story');
+    await injectAxe(page);
+    await checkA11y(page);
   });
 });
