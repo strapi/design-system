594c5045bc4965139fce2df4039b414efba07ffa
ronronscelestes <julie.plantey@gmail.com>
2022-03-02 09:35:53 +0100

    feedback fix on preview.js

--- packages/strapi-design-system/.storybook/preview.js
+++ packages/strapi-design-system/.storybook/preview.js
@@ -19,37 +19,19 @@ export const decorators = [
   (Story) => {
     const themeQueryURL = parse(document.location.search).theme;
 
-    let theme;
-    if(themeQueryURL === 'dark') {
-      theme = 'dark';
-    } else {
-      theme = 'light';
-    };
-    
     return (
       <>
-        {theme === 'dark' ? 
-          (<ThemeProvider theme={darkTheme}>
-            <main>
-              <VisuallyHidden>
-                {/* Necessary in order to prevent axe core from providing errors on main / heading */}
-                <h1>Storybook story</h1>
-              </VisuallyHidden>
-              <Box background='neutral100' padding={2}>
-                <Story />
-              </Box>
-            </main>
-          </ThemeProvider>) 
-          : 
-          (<ThemeProvider theme={lightTheme}>
-            <main>
-              <VisuallyHidden>
-                {/* Necessary in order to prevent axe core from providing errors on main / heading */}
-                <h1>Storybook story</h1>
-              </VisuallyHidden>
+        <ThemeProvider theme={themeQueryURL === 'dark' ? darkTheme : lightTheme}>
+          <main>
+            <VisuallyHidden>
+              {/* Necessary in order to prevent axe core from providing errors on main / heading */}
+              <h1>Storybook story</h1>
+            </VisuallyHidden>
+            <Box background='neutral100' padding={2}>
               <Story />
-            </main>
-          </ThemeProvider>)}
+            </Box>
+          </main>
+        </ThemeProvider>
       </>
     )
   },
