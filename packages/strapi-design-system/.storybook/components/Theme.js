20676703697780eddf474481a5bb554eda4dc256
ronronscelestes <julie.plantey@gmail.com>
2022-03-07 11:05:48 +0100

    neutral0 background to each story - prevent e2e test crashing in dark mode

--- packages/strapi-design-system/.storybook/components/Theme.js
+++ packages/strapi-design-system/.storybook/components/Theme.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from '../../src/ThemeProvider';
+import { Box } from '../../src/Box';
 import { lightTheme } from '../../src/themes/lightTheme';
 import { darkTheme } from '../../src/themes/darkTheme';
 import { useThemeType } from '../hooks/useThemeType';
@@ -8,7 +9,9 @@ const Theme = ({ children }) => {
   const themeType = useThemeType();
 
   return (
-    <ThemeProvider theme={themeType === 'dark' ? darkTheme : lightTheme}>{children}</ThemeProvider>
+    <ThemeProvider theme={themeType === 'dark' ? darkTheme : lightTheme}>
+      <Box padding={2} background='neutral0'>{children}</Box>
+    </ThemeProvider>
   )
 };
 
