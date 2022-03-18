4add6205f42b62c9cfa4e9c488c8f64ef4727c09
ronronscelestes <julie.plantey@gmail.com>
2022-03-18 18:21:50 +0100

    refacto Theme using storybook-dark-mode current mode

--- packages/strapi-design-system/.storybook/components/Theme.js
+++ packages/strapi-design-system/.storybook/components/Theme.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDarkMode } from 'storybook-dark-mode';
+import { parse } from 'qs';
 import { ThemeProvider } from '../../src/ThemeProvider';
 import { Box } from '../../src/Box';
 import { lightTheme } from '../../src/themes/lightTheme';
 import { darkTheme } from '../../src/themes/darkTheme';
-import { useThemeType } from '../hooks/useThemeType';
+
+const themeQueryURL = parse(document.location.search).theme;
 
 const Theme = ({ children }) => {
-  const themeType = useThemeType();
+  const isDarkAddon = useDarkMode();
+  const [isDark, setIsDark] = useState(themeQueryURL || isDarkAddon);
+
+  useEffect(() => {
+    if(!themeQueryURL && isDarkAddon !== isDark) {
+      setIsDark(isDarkAddon);
+    }
+  }, [isDarkAddon, isDark]);
 
   return (
-    <ThemeProvider theme={themeType === 'dark' ? darkTheme : lightTheme}>
-      <Box padding={2} background='neutral0'>{children}</Box>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      {children}
     </ThemeProvider>
   )
 };
