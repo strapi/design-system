9f0d3ca29282383132977a8ba9cb89d6c760c921
Mattias van den Belt <mattiasvandenbelt@gmail.com>
2022-03-20 11:35:03 +0100

    Add width and height to Icon Component

--- packages/strapi-design-system/src/Icon/Icon.js
+++ packages/strapi-design-system/src/Icon/Icon.js
@@ -4,6 +4,9 @@ import { Box } from '../Box';
 import styled from 'styled-components';
 
 const IconWrapper = styled(Box)`
+  width: ${({ theme, width }) => theme.spaces[width]};
+  height: ${({ theme, height }) => theme.spaces[height]};
+
   path {
     fill: ${({ color, theme }) => theme.colors[color]};
   }
@@ -19,9 +22,13 @@ Icon.displayName = 'Icon';
 Icon.defaultProps = {
   color: 'neutral600',
   colors: () => undefined,
+  height: 1,
+  width: 1,
 };
 
 Icon.propTypes = {
   color: PropTypes.string,
   colors: PropTypes.func,
+  height: PropTypes.number,
+  width: PropTypes.number,
 };
