56b9a91a93f56b476c90fc40f35cb3445a691fe6
Gustav Hansen <gustav.hansen@strapi.io>
2022-07-15 16:21:01 +0200

    CrumbSimpleMenu: Improve component rendering

--- packages/strapi-design-system/src/v2/Breadcrumbs/CrumbSimpleMenu.js
+++ packages/strapi-design-system/src/v2/Breadcrumbs/CrumbSimpleMenu.js
@@ -1,18 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import styled from 'styled-components';
 
-import { IconButton } from '../../IconButton';
 import { SimpleMenu } from '../SimpleMenu';
 
 import CarretDown from '@strapi/icons/CarretDown';
 
-const IconButtonCustom = styled(IconButton)`
-  height: ${({ theme }) => theme.spaces[3]};
-`;
-
 export const CrumbSimpleMenu = ({ children, ...props }) => (
-  <SimpleMenu noBorder as={IconButtonCustom} icon={<CarretDown />} {...props}>
+  <SimpleMenu noBorder icon={<CarretDown />} size="S" {...props}>
     {children}
   </SimpleMenu>
 );
