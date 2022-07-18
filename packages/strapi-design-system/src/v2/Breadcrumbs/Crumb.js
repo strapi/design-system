0e2854f8e913b61e75d4afa9a10fce5487f14244
Gustav Hansen <gustav.hansen@strapi.io>
2022-07-18 08:55:10 +0200

    Crumb: add isCurrent prop to differtiate the current page

--- packages/strapi-design-system/src/v2/Breadcrumbs/Crumb.js
+++ packages/strapi-design-system/src/v2/Breadcrumbs/Crumb.js
@@ -3,14 +3,25 @@ import PropTypes from 'prop-types';
 
 import { Typography } from '../../Typography';
 
-export const Crumb = ({ children, ...props }) => (
-  <Typography variant="pi" textColor="neutral800" fontWeight="bold" {...props}>
+export const Crumb = ({ children, isCurrent, ...props }) => (
+  <Typography
+    variant="pi"
+    textColor="neutral800"
+    fontWeight={isCurrent ? 'bold' : 'normal'}
+    aria-current={isCurrent}
+    {...props}
+  >
     {children}
   </Typography>
 );
 
 Crumb.displayName = 'Crumb';
 
+Crumb.defaultProps = {
+  isCurrent: false,
+};
+
 Crumb.propTypes = {
   children: PropTypes.string.isRequired,
+  isCurrent: PropTypes.bool,
 };
