775ca919dbd90a9b9e037fa978672b867787f854
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-09-16 16:12:43 +0100

    fix: use floating-ui to handle popover positioning

--- packages/strapi-design-system/src/Popover/Popover.js
+++ packages/strapi-design-system/src/Popover/Popover.js
@@ -1,59 +1,15 @@
-import React, { useRef, useState } from 'react';
+import * as React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { useFloating, flip, shift, offset, autoUpdate } from '@floating-ui/react-dom';
+
 import { Box } from '../Box';
 import { Portal } from '../Portal';
+
 import { useIntersection } from '../helpers/useIntersection';
-import { useResizeObserver } from '../helpers/useResizeObserver';
-
-export const position = (source, popover, fullWidth, centered, spacing = 0) => {
-  const rect = source.getBoundingClientRect();
-  let top = rect.top + rect.height + window.pageYOffset + spacing;
-  let left = rect.left + window.pageXOffset;
-
-  if (!popover) {
-    return {
-      left,
-      top,
-      width: fullWidth ? rect.width : undefined,
-    };
-  }
-
-  const popoverRect = popover.getBoundingClientRect();
-
-  if (centered) {
-    const popoverBorderPadding = 10;
-    const popoverTotalWidth = popoverRect.width + popoverBorderPadding;
-    const widthDifference = (rect.width - popoverTotalWidth) / 2;
-
-    left = rect.left + widthDifference + window.pageXOffset;
-  }
-
-  // if popover overflows left or right viewport
-  if (popoverRect.left < 0) {
-    left = rect.left + window.pageXOffset;
-  } else if (popoverRect.left + popoverRect.width > window.innerWidth) {
-    left = window.innerWidth - popoverRect.width - 20;
-  }
-
-  const windowSizeAtPosition = window.innerHeight + window.pageYOffset;
-
-  // if popover overflows bottom of viewport
-  if (top + popoverRect.height + spacing > windowSizeAtPosition) {
-    const popoverBorderPadding = 10;
-    top = window.pageYOffset + rect.top - popoverRect.height - popoverBorderPadding - spacing;
-  }
-
-  return {
-    left,
-    top,
-    width: fullWidth ? rect.width : undefined,
-  };
-};
 
 const PopoverWrapper = styled(Box)`
   box-shadow: ${({ theme }) => theme.shadows.filterShadow};
-  position: absolute;
   z-index: 4;
   border: 1px solid ${({ theme }) => theme.colors.neutral150};
   background: ${({ theme }) => theme.colors.neutral0};
@@ -82,28 +38,49 @@ const PopoverScrollable = styled(Box)`
 `;
 
 const PopoverContent = ({ source, children, spacing, fullWidth, onReachEnd, intersectionId, centered, ...props }) => {
-  const popoverRef = useRef(null);
-  const [{ left, top, width }, setPosition] = useState(
-    position(source.current, popoverRef.current, fullWidth, centered, spacing),
-  );
+  const popoverRef = React.useRef(null);
+  const [width, setWidth] = React.useState(undefined);
+  const { x, y, reference, floating, strategy } = useFloating({
+    strategy: 'fixed',
+    placement: centered ? 'bottom' : 'bottom-start',
+    middleware: [
+      offset({
+        mainAxis: spacing,
+      }),
+      shift(),
+      flip(),
+    ],
+    whileElementsMounted: autoUpdate,
+  });
 
-  useResizeObserver([source, popoverRef], () =>
-    setPosition(position(source.current, popoverRef.current, fullWidth, centered, spacing)),
-  );
+  React.useLayoutEffect(() => {
+    reference(source.current);
+  }, [source]);
+
+  React.useLayoutEffect(() => {
+    if (fullWidth) {
+      setWidth(source.current.offsetWidth);
+    }
+  }, [fullWidth]);
 
   useIntersection(popoverRef, onReachEnd, {
     selectorToWatch: `#${intersectionId}`,
     skipWhen: !intersectionId || !onReachEnd,
   });
 
-  const style = {
-    left: `${left}px`,
-    top: `${top}px`,
-    width: width ? `${width}px` : undefined,
-  };
-
   return (
-    <PopoverWrapper style={style} hasRadius background="neutral0" padding={1}>
+    <PopoverWrapper
+      ref={floating}
+      style={{
+        left: x,
+        top: y,
+        position: strategy,
+        width,
+      }}
+      hasRadius
+      background="neutral0"
+      padding={1}
+    >
       <PopoverScrollable ref={popoverRef} {...props}>
         {children}
         {intersectionId && onReachEnd && <Box id={intersectionId} width="100%" height="1px" />}
