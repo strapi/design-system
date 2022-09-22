168687d5dfb23a9f9c4e69e1886d47a78e53eb54
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-09-22 17:02:27 +0100

    refactor: RawCell aria logic

--- packages/strapi-design-system/src/RawTable/RawCell.js
+++ packages/strapi-design-system/src/RawTable/RawCell.js
@@ -1,51 +1,89 @@
-import React, { useLayoutEffect, useRef } from 'react';
+import React, { useLayoutEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
+
 import { Box } from '../Box';
 import { getFocusableNodes } from '../helpers/getFocusableNodes';
 
-export const RawTh = ({ isFocusable, ...props }) => {
-  const thRef = useRef(null);
+import { useTable } from './RawTableContext';
+
+export const RawTh = (props) => <RawTd {...props} as="th" />;
+
+export const RawTd = ({ coords, as, ...props }) => {
+  const tdRef = useRef(null);
+  const { rowIndex, colIndex } = useTable();
+  const [isActive, setIsActive] = useState(false);
+
+  /** @type {import("react").KeyboardEventHandler<HTMLTableCellElement> } */
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter' && !isActive) {
+      setIsActive(true);
+      /**
+       * Cells should be "escapeable" with the escape key or enter key
+       */
+    } else if ((e.key === 'Escape' || e.key === 'Enter') && isActive) {
+      setIsActive(false);
+      /**
+       * Refocus the cell
+       */
+      tdRef.current.focus();
+    } else if (isActive) {
+      /**
+       * This stops the table navigation from working
+       */
+      e.stopPropagation();
+    }
+  };
+
+  const isFocused = rowIndex === coords.row - 1 && colIndex === coords.col - 1;
 
   useLayoutEffect(() => {
-    const focusableNodes = getFocusableNodes(thRef.current, true);
-    if (focusableNodes.length === 0) {
-      thRef.current.setAttribute('tabIndex', isFocusable ? 0 : -1);
-    } else {
-      focusableNodes.forEach((node) => node.setAttribute('tabIndex', isFocusable ? 0 : -1));
-    }
-  }, [isFocusable]);
-
-  return <Box as="th" ref={thRef} {...props} />;
-};
-
-export const RawTd = ({ isFocusable, ...props }) => {
-  const tdRef = useRef(null);
+    tdRef.current.setAttribute('tabIndex', !isActive && isFocused ? 0 : -1);
+  }, [isActive, isFocused]);
 
   useLayoutEffect(() => {
     const focusableNodes = getFocusableNodes(tdRef.current, true);
+    focusableNodes.forEach((node, index) => {
+      node.setAttribute('tabIndex', isActive ? 0 : -1);
+      /**
+       * When a cell is active we want to focus the
+       * first focusable element simulating a focus trap
+       */
+      if (isActive && index === 0) {
+        node.focus();
+      }
+    });
+  }, [isActive]);
 
-    if (focusableNodes.length === 0) {
-      tdRef.current.setAttribute('tabIndex', isFocusable ? 0 : -1);
-    } else {
-      focusableNodes.forEach((node) => node.setAttribute('tabIndex', isFocusable ? 0 : -1));
-    }
-  }, [isFocusable]);
-
-  return <Box as="td" ref={tdRef} {...props} />;
+  return <Box as={as ? as : 'td'} ref={tdRef} onKeyDown={handleKeyDown} {...props} />;
 };
 
 RawTh.defaultProps = {
-  isFocusable: false,
+  coords: {},
 };
 
 RawTh.propTypes = {
-  isFocusable: PropTypes.bool,
+  ['aria-colindex']: PropTypes.number.isRequired,
+  /**
+   * Position of the cell in the table
+   */
+  coords: PropTypes.shape({
+    col: PropTypes.number,
+    row: PropTypes.number,
+  }),
 };
 
 RawTd.defaultProps = {
-  isFocusable: false,
+  coords: {},
 };
 
 RawTd.propTypes = {
-  isFocusable: PropTypes.bool,
+  ['aria-colindex']: PropTypes.number.isRequired,
+  as: PropTypes.oneOf(['td', 'th']),
+  /**
+   * Position of the cell in the table
+   */
+  coords: PropTypes.shape({
+    col: PropTypes.number,
+    row: PropTypes.number,
+  }),
 };
