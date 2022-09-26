44754fb3a41912c97ab0d1809c4408ac7a055864
Simone Taeggi <startae14@gmail.com>
2022-09-26 11:37:55 +0200

    Add e2e tests DateTimePicker

---
+++ packages/strapi-design-system/src/DateTimePicker/__tests__/DateTimePicker.e2e.js
@@ -0,0 +1,51 @@
+const { injectAxe, checkA11y } = require('axe-playwright');
+
+const { test } = require('@playwright/test');
+
+test.describe.parallel('DateTimePicker', () => {
+  test.describe('light mode', () => {
+    test('base A11y', async ({ page }) => {
+      // This is the URL of the Storybook Iframe
+      await page.goto('/iframe.html?id=design-system-components-datetimepicker--base&viewMode=story');
+      await injectAxe(page);
+      await checkA11y(page);
+    });
+
+    test('error A11y', async ({ page }) => {
+      // This is the URL of the Storybook Iframe
+      await page.goto('/iframe.html?id=design-system-components-datetimepicker--error&viewMode=story');
+      await injectAxe(page);
+      await checkA11y(page);
+    });
+
+    test('initial data A11y', async ({ page }) => {
+      // This is the URL of the Storybook Iframe
+      await page.goto('/iframe.html?id=design-system-components-datetimepicker--intial-data&viewMode=story');
+      await injectAxe(page);
+      await checkA11y(page);
+    });
+  });
+
+  test.describe('dark mode', () => {
+    test('base A11y', async ({ page }) => {
+      // This is the URL of the Storybook Iframe
+      await page.goto('/iframe.html?id=design-system-components-datetimepicker--base&viewMode=story');
+      await injectAxe(page);
+      await checkA11y(page);
+    });
+
+    test('error A11y', async ({ page }) => {
+      // This is the URL of the Storybook Iframe
+      await page.goto('/iframe.html?id=design-system-components-datetimepicker--error&viewMode=story');
+      await injectAxe(page);
+      await checkA11y(page);
+    });
+
+    test('initial data A11y', async ({ page }) => {
+      // This is the URL of the Storybook Iframe
+      await page.goto('/iframe.html?id=design-system-components-datetimepicker--intial-data&viewMode=story');
+      await injectAxe(page);
+      await checkA11y(page);
+    });
+  });
+});
