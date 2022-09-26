b4e79bf23178a89f0701a506f4408bb8c383ee92
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-09-26 10:30:55 +0100

    test: add tests to DatePicker around locale prop

---
+++ packages/strapi-design-system/src/DatePicker/__tests__/DatePicker.spec.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { DatePicker } from '../DatePicker';
+
+import { lightTheme } from '../../themes';
+import { ThemeProvider } from '../../ThemeProvider';
+
+describe('DatePicker', () => {
+  describe('Locale prop', () => {
+    const renderDate = (locale) =>
+      render(
+        <ThemeProvider theme={lightTheme}>
+          <DatePicker
+            label="date"
+            locale={locale}
+            selectedDateLabel={(formattedDate) => `Date picker, current is ${formattedDate}`}
+            selectedDate={new Date('Tue Sep 06 2022 00:00:00 GMT+0100 (British Summer Time)')}
+          />
+        </ThemeProvider>,
+      );
+
+    it('should format by EN locale by default', () => {
+      renderDate();
+
+      const input = screen.getByRole('textbox');
+
+      expect(input.value).toMatchInlineSnapshot('"9/6/2022"');
+    });
+
+    it('should format by the DE locale when passed', () => {
+      renderDate('de-DE');
+
+      const input = screen.getByRole('textbox');
+
+      expect(input.value).toMatchInlineSnapshot('"6.9.2022"');
+    });
+  });
+});
