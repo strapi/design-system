211c870ca4a4bf3cf3bf6febf08049cf10184de0
Prasad Patewar <prasadpatewar39@gmail.com>
2022-10-07 22:30:04 +0530

    Removed ellipsis style from Box and applied to only carousel image

--- packages/strapi-design-system/src/Box/Box.js
+++ packages/strapi-design-system/src/Box/Box.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import handleResponsiveValues from '../helpers/handleResponsiveValues';
-import { ellipsisStyle, handleColor } from '../Typography/utils';
 import { boxPropTypes, boxDefaultProps } from './BoxProps';
 
 /**
@@ -10,20 +9,7 @@ const transientProps = {
   color: true,
 };
 
-// ImageBase has been added here to check if Box is used as "img" tag
-// If so, to handle alt text overflow following properties has been added
-const ImageBase = styled.div`
-  ${({ as, theme, color }) => {
-    if (as === 'img')
-      return `
-    width: stretch;
-    ${ellipsisStyle({ ellipsis: true })}
-    color: ${handleColor({ theme, textColor: color })};
-    `;
-  }};
-`;
-
-export const Box = styled(ImageBase).withConfig({
+export const Box = styled.div.withConfig({
   shouldForwardProp: (prop, defPropValFN) => !transientProps[prop] && defPropValFN(prop),
 })`
   // Font
--- packages/strapi-design-system/src/CarouselInput/CarouselImage.js
+++ packages/strapi-design-system/src/CarouselInput/CarouselImage.js
@@ -3,17 +3,26 @@ import PropTypes from 'prop-types';
 import { Box } from '../Box';
 import styled from 'styled-components';
 import { Tooltip } from '../Tooltip';
+import { ellipsisStyle, handleColor } from '../Typography/utils';
 
 // ImageBase is needed since tooltip requires box level element to work upon
 // Also img component can not be directly worked with tooltip otherwise it will span the whole window
-const ImageBase = styled.div`
+const ImageBase = styled(Box)`
   display: grid;
+`;
+
+const StyledImage = styled(Box)`
+  ${({ theme, color }) => `
+  width: stretch;
+  ${ellipsisStyle({ ellipsis: true })}
+  color: ${handleColor({ theme, textColor: color })};
+  `};
 `;
 
 export const CarouselImage = (props) => (
   <Tooltip description={props.alt ?? ''}>
     <ImageBase>
-      <Box as="img" {...props} />
+      <StyledImage as="img" {...props} />
     </ImageBase>
   </Tooltip>
 );
