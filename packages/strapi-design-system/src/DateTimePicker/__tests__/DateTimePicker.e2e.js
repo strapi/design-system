eee2cdbbb8d96d7f9b14462c18963e6894d247fc
Simone Taeggi <startae14@gmail.com>
2022-10-17 11:53:15 +0200

    e2e test to control change the date with initial data and change the time without initial data

--- packages/strapi-design-system/src/DateTimePicker/__tests__/DateTimePicker.e2e.js
+++ packages/strapi-design-system/src/DateTimePicker/__tests__/DateTimePicker.e2e.js
@@ -1,40 +1,80 @@
-const { injectAxe, checkA11y } = require('axe-playwright');
+const { injectAxe } = require('axe-playwright');
 
 const { test, expect } = require('@playwright/test');
 
 test.describe.parallel('DateTimePicker', () => {
-  test.beforeEach(async ({ page }) => {
-    // This is the URL of the Storybook Iframe
-    await page.goto('/iframe.html?id=design-system-components-datetimepicker--base&viewMode=story');
-    await injectAxe(page);
-  });
-  test.describe('light mode', () => {
-    test('base A11y', async ({ page }) => {
-      await checkA11y(page);
+  test.describe('without initial data', () => {
+    test.beforeEach(async ({ page }) => {
+      // This is the URL of the Storybook Iframe
+      await page.goto('/iframe.html?id=design-system-components-datetimepicker--base&viewMode=story');
+      await injectAxe(page);
+    });
+    test('change the date and see if also the time will be changed when both are empty', async ({
+      page,
+      browserName,
+    }) => {
+      test.skip(browserName === 'webkit', 'Still working on it');
+      await page.focus('[aria-labelledby="datetime-label"] > div > div:nth-child(2) > div > div');
+
+      await page.keyboard.press('Tab');
+      await page.keyboard.press('Enter');
+      await page.keyboard.press('ArrowDown');
+      await page.keyboard.press('Tab');
+      await page.keyboard.press('Tab'); // move to the Su column
+      await page.keyboard.press('ArrowDown');
+      await page.keyboard.press('ArrowDown');
+      await page.keyboard.press('ArrowDown'); // move to the third Sunday of the month
+      await page.keyboard.press('Enter'); // to select the date
+      // check if the time is selected with 00:00
+      const timePickerValue = page.locator('span:has-text("00:00")');
+      expect(await timePickerValue.innerText()).toBe('00:00');
+    });
+    test('change the time and see if also the date will be changed when both are empty', async ({
+      page,
+      browserName,
+    }) => {
+      test.skip(browserName === 'webkit', 'Still working on it');
+      await page.focus('[aria-labelledby="datetime-label"] > div > div:nth-child(2) > div');
+
+      await page.keyboard.press('Tab');
+      await page.keyboard.press('Tab');
+      await page.keyboard.press('Tab');
+      await page.keyboard.press('Enter');
+      await page.keyboard.press('ArrowDown');
+      await page.keyboard.press('ArrowDown');
+      await page.keyboard.press('ArrowDown');
+      await page.keyboard.press('Enter'); // to select the time
+      // check if the date is selected with the current date
+      const today = new Date().toLocaleDateString('en-US');
+      const timePickerValue = page.locator('[name="datetimepicker"]');
+      expect(await timePickerValue.inputValue()).toBe(today);
     });
   });
-
-  test.describe('dark mode', () => {
-    test('base A11y', async ({ page }) => {
-      await checkA11y(page);
+  test.describe('with initial data', () => {
+    test.beforeEach(async ({ page }) => {
+      // This is the URL of the Storybook Iframe
+      await page.goto('/iframe.html?id=design-system-components-datetimepicker--initial-data&viewMode=story');
+      await injectAxe(page);
     });
-  });
+    test('change the date and see if also the time will be changed when are both already initialized', async ({
+      page,
+      browserName,
+    }) => {
+      test.skip(browserName === 'webkit', 'Still working on it');
+      await page.focus('[aria-labelledby="datetime-label"] > div > div:nth-child(2) > div > div');
 
-  test('change the date and see if also the time will be changed', async ({ page, browserName }) => {
-    test.skip(browserName === 'webkit', 'Still working on it');
-    await page.focus('[aria-labelledby="datetime-label"] > div > div:nth-child(2) > div > div');
-
-    await page.keyboard.press('Tab');
-    await page.keyboard.press('Enter');
-    await page.keyboard.press('ArrowDown');
-    await page.keyboard.press('Tab');
-    await page.keyboard.press('Tab'); // move to the Su column
-    await page.keyboard.press('ArrowDown');
-    await page.keyboard.press('ArrowDown');
-    await page.keyboard.press('ArrowDown'); // move to the third Sunday of the month
-    await page.keyboard.press('Enter'); // to select the date
-    // check if the time is selected with 00:00
-    const timePickerValue = page.locator('span:has-text("00:00")');
-    expect(await timePickerValue.innerText()).toBe('00:00');
+      await page.keyboard.press('Tab');
+      await page.keyboard.press('Enter');
+      await page.keyboard.press('ArrowDown');
+      await page.keyboard.press('Tab');
+      await page.keyboard.press('Tab'); // move to the Su column
+      await page.keyboard.press('ArrowDown');
+      await page.keyboard.press('ArrowDown');
+      await page.keyboard.press('ArrowDown'); // move to the third Sunday of the month
+      await page.keyboard.press('Enter'); // to select the date
+      // check if the time is selected with 12:00
+      const timePickerValue = page.locator('span:has-text("12:00")');
+      expect(await timePickerValue.innerText()).toBe('12:00');
+    });
   });
 });
