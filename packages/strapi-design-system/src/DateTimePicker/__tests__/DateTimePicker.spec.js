e89b57d60b409d83aff0af63e3b2d48eb054161d
Simone Taeggi <startae14@gmail.com>
2022-10-19 23:12:40 +0200

    reduce boilerplate inside the DateTimePicker test

--- packages/strapi-design-system/src/DateTimePicker/__tests__/DateTimePicker.spec.js
+++ packages/strapi-design-system/src/DateTimePicker/__tests__/DateTimePicker.spec.js
@@ -4,36 +4,62 @@ import { DateTimePicker } from '../DateTimePicker';
 import { ThemeProvider } from '../../ThemeProvider';
 import { lightTheme } from '../../themes';
 
+const setupComponent = (props) => (
+  <ThemeProvider theme={lightTheme}>
+    <DateTimePicker {...props} />
+  </ThemeProvider>
+);
+
+const sharedProps = {
+  onChange: () => {},
+  name: 'datetimepicker',
+  label: 'Date time picker',
+  hint: 'This is a super description',
+};
+
+const snapshotProps = {
+  value: new Date('2021-10-13T10:00:00.000Z'),
+  ...sharedProps,
+};
+
+const initializationProps = {
+  value: new Date('2021-10-13T15:45:00.000Z'),
+  step: 15,
+  ...sharedProps,
+};
+
+const withNewValueInitialProps = {
+  value: new Date('2021-10-13T13:43:00.000Z'),
+  step: 15,
+  ...sharedProps,
+};
+
+const withNewValueFinalProps = {
+  value: new Date('2021-10-04T13:00:00.000Z'),
+  step: 15,
+  ...sharedProps,
+};
+
+const withEmptyValuesInitialProps = {
+  value: new Date('2021-10-13T15:43:00.000Z'),
+  step: 15,
+  ...sharedProps,
+};
+
+const withEmptyValuesFinalProps = {
+  step: 15,
+  ...sharedProps,
+};
+
 describe('DateTimePicker', () => {
   it('snapshots the component', () => {
-    const { container } = render(
-      <ThemeProvider theme={lightTheme}>
-        <DateTimePicker
-          value={new Date('2021-10-13T10:00:00.000Z')}
-          onChange={() => {}}
-          name="datetimepicker"
-          label="Date time picker"
-          hint="This is a super description"
-        />
-      </ThemeProvider>,
-    );
+    const { container } = render(setupComponent(snapshotProps));
 
     expect(container.firstChild).toMatchSnapshot();
   });
 
   it('should initialize the two inputs', () => {
-    render(
-      <ThemeProvider theme={lightTheme}>
-        <DateTimePicker
-          value={new Date('2021-10-13T15:45:00.000Z')}
-          step={15}
-          onChange={() => {}}
-          name="datetimepicker"
-          label="Date time picker"
-          hint="This is a super description"
-        />
-      </ThemeProvider>,
-    );
+    render(setupComponent(initializationProps));
 
     expect(screen.getByText('15:45')).toBeInTheDocument();
     const datepicker = screen.getByTestId('datetimepicker-date');
@@ -42,31 +68,9 @@ describe('DateTimePicker', () => {
   });
 
   it('should rerender a new value passed as props', () => {
-    const { rerender } = render(
-      <ThemeProvider theme={lightTheme}>
-        <DateTimePicker
-          value={new Date('2021-10-13T13:43:00.000Z')}
-          step={15}
-          onChange={() => {}}
-          name="datetimepicker"
-          label="Date time picker"
-          hint="This is a super description"
-        />
-      </ThemeProvider>,
-    );
+    const { rerender } = render(setupComponent(withNewValueInitialProps));
 
-    rerender(
-      <ThemeProvider theme={lightTheme}>
-        <DateTimePicker
-          value={new Date('2021-10-04T13:00:00.000Z')}
-          step={15}
-          onChange={() => {}}
-          name="datetimepicker"
-          label="Date time picker"
-          hint="This is a super description"
-        />
-      </ThemeProvider>,
-    );
+    rerender(setupComponent(withNewValueFinalProps));
 
     const datepicker = screen.getByTestId('datetimepicker-date');
 
@@ -74,30 +78,9 @@ describe('DateTimePicker', () => {
   });
 
   it('should rerender an empty value if it is passed as props', () => {
-    const { rerender } = render(
-      <ThemeProvider theme={lightTheme}>
-        <DateTimePicker
-          value={new Date('2021-10-13T15:43:00.000Z')}
-          step={15}
-          onChange={() => {}}
-          name="datetimepicker"
-          label="Date time picker"
-          hint="This is a super description"
-        />
-      </ThemeProvider>,
-    );
+    const { rerender } = render(setupComponent(withEmptyValuesInitialProps));
 
-    rerender(
-      <ThemeProvider theme={lightTheme}>
-        <DateTimePicker
-          step={15}
-          onChange={() => {}}
-          name="datetimepicker"
-          label="Date time picker"
-          hint="This is a super description"
-        />
-      </ThemeProvider>,
-    );
+    rerender(setupComponent(withEmptyValuesFinalProps));
 
     const datepicker = screen.getByTestId('datetimepicker-date');
 
