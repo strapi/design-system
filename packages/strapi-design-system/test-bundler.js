60c2297fef163f8c7de4bca715fbfa3b55d6ea65
Gustav Hansen <gustav.hansen@strapi.io>
2022-10-31 13:07:08 +0100

    Jest: Use proper resize-observer polyfill and test setup

--- packages/strapi-design-system/test-bundler.js
+++ packages/strapi-design-system/test-bundler.js
@@ -1,42 +1,35 @@
 import IntlPolyfill from 'intl';
+import ResizeObserver from 'resize-observer-polyfill';
+
 import 'intl/locale-data/jsonp/en';
 import 'jest-styled-components';
 
-if (global.Intl) {
-  Intl.NumberFormat = IntlPolyfill.NumberFormat;
-  Intl.DateTimeFormat = IntlPolyfill.DateTimeFormat;
-} else {
-  global.Intl = IntlPolyfill;
-}
-
-window.ResizeObserver = () => ({
-  observe: () => {},
-  disconnect: () => {},
-});
-
-window.IntersectionObserver = () => ({
-  observe: () => {},
-  disconnect: () => {},
-});
-
-window.matchMedia = () => ({
-  writable: true,
-  value: jest.fn().mockImplementation((query) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(),
-    removeListener: jest.fn(),
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-});
-
-const realConsoleError = console.error.bind(console);
-console.error = (msg) => {
-  // This errors pops when we try to test portals with testing-library
-  if (!msg.includes('Warning: render(): Rendering components directly into document.body is discouraged')) {
-    realConsoleError(msg);
+beforeAll(() => {
+  if (global.Intl) {
+    Intl.NumberFormat = IntlPolyfill.NumberFormat;
+    Intl.DateTimeFormat = IntlPolyfill.DateTimeFormat;
+  } else {
+    global.Intl = IntlPolyfill;
   }
-};
+
+  global.ResizeObserver = ResizeObserver;
+
+  global.IntersectionObserver = () => ({
+    observe: () => {},
+    disconnect: () => {},
+  });
+
+  global.matchMedia = () => ({
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+});
