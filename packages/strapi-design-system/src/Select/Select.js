018addc423c324967400f74694337409953845e6
Simone Taeggi <startae14@gmail.com>
2022-11-03 11:33:22 +0100

    feat: Implement the DatetimePicker component

--- packages/strapi-design-system/src/Select/Select.js
+++ packages/strapi-design-system/src/Select/Select.js
@@ -41,6 +41,7 @@ export const Select = ({
   onReachEnd,
   multi,
   required,
+  selectButtonTitle,
   size,
   startIcon,
   withTags,
@@ -53,7 +54,11 @@ export const Select = ({
 
   const labelId = `${generatedId}-label`;
   const contentId = `${generatedId}-content`;
-  const ariaDescribedBy = error ? `${generatedId}-error` : hint ? `${generatedId}-hint` : undefined;
+  const hasStringError = typeof error === 'string';
+  const errorMessageId = hasStringError ? `${generatedId}-error` : undefined;
+  const hintMessageId = hint ? `${generatedId}-hint` : undefined;
+
+  const ariaDescribedBy = errorMessageId || hintMessageId;
 
   if (withTags && !multi) {
     throw new Error('The "withTags" props can only be used when the "multi" prop is present');
@@ -149,7 +154,7 @@ export const Select = ({
 
   return (
     <Field hint={hint} error={error} id={generatedId}>
-      <Stack spacing={label || hint || error ? 1 : 0}>
+      <Stack spacing={label || hint || hasStringError ? 1 : 0}>
         {label && (
           <FieldLabel required={required} as="span" id={labelId} action={labelAction}>
             {label}
@@ -210,6 +215,7 @@ export const Select = ({
                   onClick={handleClear}
                   aria-label={clearLabel}
                   aria-disabled={disabled}
+                  title={clearLabel}
                 >
                   <Cross />
                 </IconBox>
@@ -223,6 +229,7 @@ export const Select = ({
                 onMouseDown={handleMouseDown}
                 tabIndex={-1}
                 disabled={disabled}
+                title={selectButtonTitle}
               >
                 <CarretDown />
               </CaretBox>
@@ -276,6 +283,7 @@ Select.defaultProps = {
   error: undefined,
   placeholder: 'Select...',
   required: false,
+  selectButtonTitle: 'Carret Down Button',
   size: 'M',
   startIcon: undefined,
   withTags: false,
@@ -287,7 +295,7 @@ Select.propTypes = {
   clearLabel: PropTypes.string,
   customizeContent: PropTypes.func,
   disabled: PropTypes.bool,
-  error: PropTypes.string,
+  error: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
   hint: PropTypes.oneOfType([PropTypes.string, PropTypes.node, PropTypes.arrayOf(PropTypes.node)]),
   id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   label: PropTypes.string,
@@ -298,6 +306,7 @@ Select.propTypes = {
   onReachEnd: PropTypes.func,
   placeholder: PropTypes.string,
   required: PropTypes.bool,
+  selectButtonTitle: PropTypes.string,
   size: PropTypes.oneOf(Object.keys(sizes.input)),
   startIcon: PropTypes.element,
   value: PropTypes.oneOfType([
