3747ec9bcdf339def8cca926203b26a77626001e
Gustav Hansen <gustav.hansen@strapi.io>
2022-11-14 12:07:13 +0100

    Badge: Update colors and height according to the UI kit

--- packages/strapi-design-system/src/Badge/Badge.js
+++ packages/strapi-design-system/src/Badge/Badge.js
@@ -1,37 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
 import { Flex } from '../Flex';
 import { Typography } from '../Typography';
 
+const Base = styled(Flex)`
+  border-radius: 2px;
+  height: ${16 / 16}rem;
+`;
+
 export const Badge = ({ active, textColor, backgroundColor, children, minWidth, ...props }) => {
   return (
-    <Flex
+    <Base
       inline
       alignItem="center"
       justifyContent="center"
       minWidth={minWidth}
-      padding={1}
-      background={active ? 'primary100' : backgroundColor}
-      hasRadius
+      paddingLeft={1}
+      paddingRight={1}
+      background={active ? 'primary200' : backgroundColor}
       {...props}
     >
       <Typography variant="sigma" textColor={active ? 'primary600' : textColor}>
         {children}
       </Typography>
-    </Flex>
+    </Base>
   );
 };
 
 Badge.defaultProps = {
   active: false,
-  backgroundColor: 'neutral100',
+  backgroundColor: 'neutral150',
   minWidth: 5,
   textColor: 'neutral600',
 };
 
 Badge.propTypes = {
   /**
-   * If `true`, it changes the `backgroundColor` to `primary100` and the `textColor` to `primary600`
+   * If `true`, it changes the `backgroundColor` to `primary200` and the `textColor` to `primary600`
    */
   active: PropTypes.bool,
   backgroundColor: PropTypes.string,
