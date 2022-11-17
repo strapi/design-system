48fe53a9cad2e44c1f40fac9e75fe73ec7ac1b38
Josh <37798644+joshuaellis@users.noreply.github.com>
2022-11-17 10:08:54 +0000

    fix: re-add main

--- website/components/Layout/index.js
+++ website/components/Layout/index.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import { Layout as PageLayout, ContentLayout } from '@strapi/design-system/Layout';
 import { Box } from '@strapi/design-system/Box';
 import { Stack } from '@strapi/design-system/Stack';
+import { Main } from '@strapi/design-system/Main';
 
 const Layout = ({ children, meta, navigationContent }) => {
   return (
@@ -13,26 +14,28 @@ const Layout = ({ children, meta, navigationContent }) => {
         <Head {...meta} />
         <Box padding={4}>
           <ContentLayout>
-            <Stack as="main" size={4}>
-              {children}
-            </Stack>
-            <Stack as="footer" size={7} horizontal paddingTop={10}>
-              <StyledLink size={2} href="https://strapi.io" target="_blank">
-                {`© ${new Date().getFullYear()} Strapi`}
-              </StyledLink>
-              <StyledLink size={2} href="https://strapi.io/careers" target="_blank">
-                Careers
-              </StyledLink>
-              <StyledLink size={2} href="https://strapi.io/privacy" target="_blank">
-                Privacy
-              </StyledLink>
-              <StyledLink size={2} href="https://strapi.io/pricing-self-hosted" target="_blank">
-                License
-              </StyledLink>
-              <StyledLink size={2} href="https://strapi.io" target="_blank">
-                strapi.io
-              </StyledLink>
-            </Stack>
+            <Main>
+              <Stack as="article" size={4}>
+                {children}
+              </Stack>
+              <Stack as="footer" size={7} horizontal paddingTop={10}>
+                <StyledLink size={2} href="https://strapi.io" target="_blank">
+                  {`© ${new Date().getFullYear()} Strapi`}
+                </StyledLink>
+                <StyledLink size={2} href="https://strapi.io/careers" target="_blank">
+                  Careers
+                </StyledLink>
+                <StyledLink size={2} href="https://strapi.io/privacy" target="_blank">
+                  Privacy
+                </StyledLink>
+                <StyledLink size={2} href="https://strapi.io/pricing-self-hosted" target="_blank">
+                  License
+                </StyledLink>
+                <StyledLink size={2} href="https://strapi.io" target="_blank">
+                  strapi.io
+                </StyledLink>
+              </Stack>
+            </Main>
           </ContentLayout>
         </Box>
       </PageLayout>
