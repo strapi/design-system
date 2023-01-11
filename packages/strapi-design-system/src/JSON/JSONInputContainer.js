bb06a4d78660d25ae4acec67acff2c4f331915a8
Mark Kaylor <mark.kaylor@strapi.io>
2023-01-11 12:21:35 +0100

    Move border and focus styles from container to .cm-editor

--- packages/strapi-design-system/src/JSON/JSONInputContainer.js
+++ packages/strapi-design-system/src/JSON/JSONInputContainer.js
@@ -4,16 +4,27 @@ import { Flex } from '../Flex';
 
 export const JSONInputContainer = styled(Flex)`
   line-height: ${({ theme }) => theme.lineHeights[2]};
-  // inputFocusStyle will receive hasError prop
-  ${inputFocusStyle()}
 
   .cm-editor {
     /* 
       Hard coded since the color is the same between themes,
       theme.colors.neutral800 changes between themes 
     */
-    width: 100%;
     background-color: #32324d;
+    width: 100%;
+    outline: none;
+    border: 1px solid ${({ theme, hasError }) => (hasError ? theme.colors.danger600 : theme.colors.neutral200)};
+    // inputFocusStyle will receive hasError prop
+    ${inputFocusStyle()}
+
+    .cm-scroller {
+      overflow: auto;
+    }
+  }
+
+  .cm-editor,
+  .cm-scroller {
+    border-radius: ${({ theme }) => theme.borderRadius};
   }
 
   .cm-gutters,
@@ -23,15 +34,5 @@ export const JSONInputContainer = styled(Flex)`
       theme.colors.neutral700 changes between themes 
     */
     background-color: #4a4a6a;
-  }
-
-  & > .cm-editor {
-    outline: none;
-    border-radius: ${({ theme }) => theme.borderRadius};
-
-    & > .cm-scroller {
-      overflow: auto;
-      border-radius: ${({ theme }) => theme.borderRadius};
-    }
   }
 `;
