a781a1d94c426979df01cc95faad4b5f1b81dadd
Mark Kaylor <mark.kaylor@strapi.io>
2023-01-11 12:48:50 +0100

    Fix comment

--- packages/strapi-design-system/src/JSON/JSONInputContainer.js
+++ packages/strapi-design-system/src/JSON/JSONInputContainer.js
@@ -14,7 +14,7 @@ export const JSONInputContainer = styled(Flex)`
     width: 100%;
     outline: none;
     border: 1px solid ${({ theme, hasError }) => (hasError ? theme.colors.danger600 : theme.colors.neutral200)};
-    // inputFocusStyle will receive hasError prop
+    /* inputFocusStyle will receive hasError prop */
     ${inputFocusStyle()}
   }
 
