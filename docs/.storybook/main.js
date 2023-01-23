b30ff6c2d83995bfe967807c05053ef34396dd46
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-01-23 13:56:38 +0000

    chore: fix tests

--- docs/.storybook/main.js
+++ docs/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-dark-mode'],
@@ -6,6 +8,14 @@ module.exports = {
     builder: 'webpack5',
   },
   webpackFinal: (config) => {
+    /**
+     * These are required to ensure we're using the correct version
+     * in storybook so `NavLink` doesn't break for example.
+     */
+    config.resolve.alias['react-router'] = path.resolve(__dirname, '../node_modules/react-router');
+    config.resolve.alias['react-router-dom'] = path.resolve(__dirname, '../node_modules/react-router-dom');
+    config.resolve.alias['styled-components'] = path.resolve(__dirname, '../node_modules/styled-components');
+
     /**
      * This is used to avoid webpack import errors where
      * the origin is strict EcmaScript Module.
--- packages/strapi-design-system/src/Link/Link.js
+++ packages/strapi-design-system/src/Link/Link.js
@@ -2,8 +2,8 @@ import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { ExternalLink } from '@strapi/icons';
-
 import { NavLink } from 'react-router-dom';
+
 import { Typography } from '../Typography';
 import { Box } from '../Box';
 import { buttonFocusStyle } from '../themes/utils';
