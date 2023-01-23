482b5e89e0d8732f66d8b3477fda0a0ff188d156
Julie Plantey <julie.plantey@gmail.com>
2023-01-23 16:59:52 +0100

    add future support for React 18 useId

--- packages/strapi-design-system/src/helpers/useId.ts
+++ packages/strapi-design-system/src/helpers/useId.ts
@@ -1,11 +1,14 @@
-/**
- * TODO: This should be moved to the `hooks` folder
- */
-import { useRef } from 'react';
+import React, { useRef } from 'react';
 import { genId } from './genId';
 
+// Inspired by radix-ui useId hook https://github.com/radix-ui/primitives/blob/main/packages/react/id/src/id.tsx
+const useReactId = React['useId'.toString()] || (() => undefined);
+
 export const useId = (initialId?: string) => {
-  const idRef = useRef(initialId || genId());
+  const reactId = useReactId();
+  // If no initialId and no reactId, then we fallback to our own getId implementation
+  // To support of React versions older than 18
+  const idRef = useRef<string>(initialId || reactId || genId());
 
   return idRef.current;
 };
