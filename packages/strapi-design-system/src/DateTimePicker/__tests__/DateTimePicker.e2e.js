8799ea6e7193aa50bef4ab5606f44dc9196f054e
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-01-25 17:33:40 +0000

    refactor: all the e2e tests for DateTimePicker

--- packages/strapi-design-system/src/DateTimePicker/__tests__/DateTimePicker.e2e.js
+++ packages/strapi-design-system/src/DateTimePicker/__tests__/DateTimePicker.e2e.js
@@ -6,98 +6,84 @@ test.describe.parallel('DateTimePicker', () => {
       // This is the URL of the Storybook Iframe
       await page.goto('/iframe.html?id=design-system-components-datetimepicker--base&viewMode=story');
     });
-    test('change the date and see if also the time will be changed when both are empty', async ({
+
+    test('when a date is entered the time picker is automatically filled in with 00:00 assuming it is empty', async ({
       page,
-      browserName,
     }) => {
-      test.skip(browserName === 'webkit', 'Still working on it');
-      await page.click('[aria-labelledby^="datetime-label"] > div > div:nth-child(2) > div > div');
-      await page.click('tr[aria-rowindex="4"] td[aria-colindex="4"]');
+      await page.getByLabel('Date time picker').click();
+      await page.getByRole('button', { name: '1/18/2023' }).click();
+
       // check if the time is selected with 00:00
-      const timePickerValue = page.locator('button[data-testid="datetimepicker-time"] + div span');
-      expect(await timePickerValue.innerText()).toBe('00:00');
+      const timePickerValue = await page.getByRole('combobox').allTextContents();
+
+      expect(timePickerValue).toContain('00:00');
     });
-    test('change the time and see if also the date will be changed when both are empty', async ({
+
+    test('when a time is entered the date picker is automatically filled in with todays date assuming it is empty', async ({
       page,
-      browserName,
     }) => {
-      test.skip(browserName === 'webkit', 'Still working on it');
-      await page.click('button[data-testid="datetimepicker-time"]');
-      await page.click('div[data-react-portal="true"] li[data-strapi-value="00:03"]');
+      await page.getByRole('combobox').click();
+      await page.getByRole('option', { name: '00:03' }).click();
       // check if the date is selected with the current date
       const today = new Date().toLocaleDateString('en-US');
-      const datePickerValue = page.locator('[name="datetimepicker"]');
-      expect(await datePickerValue.inputValue()).toBe(today);
+
+      const datePickerValue = await page.getByLabel('Date time picker').inputValue();
+
+      expect(datePickerValue).toBe(today);
     });
   });
+
   test.describe('with initial data', () => {
     test.beforeEach(async ({ page }) => {
       // This is the URL of the Storybook Iframe
       await page.goto('/iframe.html?id=design-system-components-datetimepicker--initial-data&viewMode=story');
     });
-    test('change the date and see if also the time will be changed when are both already initialized', async ({
-      page,
-      browserName,
-    }) => {
-      test.skip(browserName === 'webkit', 'Still working on it');
-      const timePickerInitialValue = await page
-        .locator('button[data-testid="datetimepicker-time"] + div span')
-        .innerText();
-      await page.click('[aria-labelledby^="datetime-label"] > div > div:nth-child(2) > div > div');
-      await page.click('tr[aria-rowindex="4"] td[aria-colindex="4"]');
-      // check if the time is selected with 12:00
-      const timePickerValue = page.locator('button[data-testid="datetimepicker-time"] + div span');
-      expect(await timePickerValue.innerText()).toBe(timePickerInitialValue);
+
+    test('when the date is changed the time picker does not change assuming it has a value', async ({ page }) => {
+      const [originalTimePickerValue] = await page.getByRole('combobox').allTextContents();
+
+      await page.getByLabel('Date time picker').click();
+      await page.getByRole('button', { name: '10/31/2021' }).click();
+
+      const [newTimePickerValue] = await page.getByRole('combobox').allTextContents();
+      expect(originalTimePickerValue).toBe(newTimePickerValue);
     });
 
-    test('change the time and see if also the date will be changed when are both already initialized', async ({
-      page,
-      browserName,
-    }) => {
-      test.skip(browserName === 'webkit', 'Still working on it');
-      await page.click('button[data-testid="datetimepicker-time"]');
-      await page.click('div[data-react-portal="true"] li[data-strapi-value="08:03"]');
-      const datePickerValue = page.locator('[name="datetimepicker"]');
-      expect(await datePickerValue.inputValue()).toBe('10/13/2021');
+    test('when the time is changed the data picker does not change assuming it has a value', async ({ page }) => {
+      const originalDatePickerValue = await page.getByLabel('Date time picker').inputValue();
+
+      await page.getByRole('combobox').click();
+      await page.getByRole('option', { name: '00:03' }).click();
+
+      const newDatePickerValue = await page.getByLabel('Date time picker').inputValue();
+
+      expect(originalDatePickerValue).toBe(newDatePickerValue);
     });
 
-    test('clear the date and see if also the time will be cleared when are both already initialized', async ({
-      page,
-      browserName,
-    }) => {
-      test.skip(browserName === 'webkit', 'Still working on it');
-      await page.focus('[aria-labelledby^="datetime-label"] > div > div:nth-child(2) > div');
+    test('when the date is cleared, the time picker should also be cleared', async ({ page }) => {
+      await page.getByRole('button', { name: 'Clear' }).first().click();
 
-      await page.keyboard.press('Tab');
-      await page.keyboard.press('Tab');
-      await page.keyboard.press('Tab');
-      await page.keyboard.press('Enter');
-      const datePickerValue = page.locator('[name="datetimepicker"]');
-      expect(await datePickerValue.inputValue()).toBe('');
-      const timePickerValue = page.locator(
-        '[aria-labelledby^="datetime-label"] > div > div:nth-child(2) > div:nth-child(2) > div > div > div:nth-child(2) span',
-      );
-      expect(await timePickerValue.innerText()).toBe('--:--');
+      const datePickerValue = await page.getByLabel('Date time picker').inputValue();
+
+      expect(datePickerValue).toBe('');
+
+      const [timePickerValue] = await page.getByRole('combobox').allTextContents();
+
+      expect(timePickerValue).toBe('--:--');
     });
-    test('clear the time and see if the date remains the same when are both already initialized', async ({
-      page,
-      browserName,
-    }) => {
-      test.skip(browserName === 'webkit', 'Still working on it');
-      await page.focus('[aria-labelledby^="datetime-label"] > div > div:nth-child(2) > div');
 
-      await page.keyboard.press('Tab');
-      await page.keyboard.press('Tab');
-      await page.keyboard.press('Tab');
-      await page.keyboard.press('Tab');
-      await page.keyboard.press('Tab');
-      await page.keyboard.press('Enter');
-      const datePickerValue = page.locator('[name="datetimepicker"]');
-      expect(await datePickerValue.inputValue()).toBe('10/13/2021');
-      const timePickerValue = page.locator(
-        '[aria-labelledby^="datetime-label"] > div > div:nth-child(2) > div:nth-child(2) > div > div > div:nth-child(2) span',
-      );
-      expect(await timePickerValue.innerText()).toBe('00:00');
+    test('clear the time and see if the date remains the same when are both already initialized', async ({ page }) => {
+      const originalDatePickerValue = await page.getByLabel('Date time picker').inputValue();
+
+      await page.getByRole('button', { name: 'Clear' }).nth(1).click();
+
+      const newDatePickerValue = await page.getByLabel('Date time picker').inputValue();
+
+      expect(originalDatePickerValue).toBe(newDatePickerValue);
+
+      const [timePickerValue] = await page.getByRole('combobox').allTextContents();
+
+      expect(timePickerValue).toBe('00:00');
     });
   });
 });
