08a34b047bd7053e6369afa4c1f2ea70dd5bff44
Julie Plantey <julie.plantey@gmail.com>
2023-01-26 13:12:50 +0100

    fix: stringify initialId in useId

--- packages/strapi-design-system/src/helpers/useId.ts
+++ packages/strapi-design-system/src/helpers/useId.ts
@@ -5,7 +5,7 @@ import React, { useLayoutEffect, useState } from 'react';
 const useReactId = (React as any)['useId'.toString()] || (() => undefined);
 let count = 0;
 
-export const useId = (initialId?: string) => {
+export const useId = (initialId?: string | number | undefined): string => {
   const [id, setId] = useState(useReactId());
 
   // React versions older than 18 will have client-side ids only.
@@ -13,5 +13,5 @@ export const useId = (initialId?: string) => {
     if (!initialId) setId((reactId) => reactId ?? String(count++));
   }, [initialId]);
 
-  return initialId || (id ? id : '');
+  return initialId?.toString() ?? (id ? id : '');
 };
