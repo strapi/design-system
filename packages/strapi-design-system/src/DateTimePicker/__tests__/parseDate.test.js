93b8e32ca04d6fef3f032f44c2f7588760ffb592
Josh <37798644+joshuaellis@users.noreply.github.com>
2023-01-27 09:47:40 +0000

    fix: parseDateTest

--- packages/strapi-design-system/src/DateTimePicker/__tests__/parseDate.test.js
+++ packages/strapi-design-system/src/DateTimePicker/__tests__/parseDate.test.js
@@ -5,6 +5,6 @@ describe('parseDate', () => {
     expect(parseDate('2021-11-05T15:37:29.592Z') instanceof Date).toBeTruthy();
   });
   it('should return null if the passed params has not the right format', () => {
-    expect(parseDate('test')).toBeUndefined();
+    expect(parseDate('test')).toBeNull();
   });
 });
