16fb8745b8486e889d5b05ab2b1d89c7b43d5c6c
Gustav Hansen <gustav.hansen@strapi.io>
2023-02-02 11:52:08 +0100

    Box: Refactor specs, add transient prop tests

--- packages/strapi-design-system/src/Box/__tests__/Box.spec.tsx
+++ packages/strapi-design-system/src/Box/__tests__/Box.spec.tsx
@@ -1,36 +1,32 @@
-import * as React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
 import { Box } from '../Box';
 import { lightTheme } from '../../themes';
 
+const setup = (props = {}) =>
+  render(
+    <ThemeProvider theme={lightTheme}>
+      <Box {...props} />
+    </ThemeProvider>,
+  );
+
 describe('Box', () => {
   it.each(['color', 'background'])('retrieves the theme value corresponding to the %s props', (colorProp) => {
-    const props = { [colorProp]: 'primary500' };
-
-    render(
-      <ThemeProvider theme={lightTheme}>
-        <Box {...props}>Hello world</Box>
-      </ThemeProvider>,
-    );
-
-    const el = screen.getByText('Hello world');
-    expect(el).toHaveStyle(`${colorProp}: #7b79ff`);
+    const { container } = setup({ [colorProp]: 'primary500' });
+    expect(container.children[0]).toHaveStyle(`${colorProp}: #7b79ff`);
   });
 
   it.each(['padding', 'paddingTop', 'paddingRight', 'paddingBottom', 'paddingLeft'])(
     'retrieves the theme value corresponding to the %s props',
     (spacingProp) => {
-      const props = { [spacingProp]: 4 };
-
-      render(
-        <ThemeProvider theme={lightTheme}>
-          <Box {...props}>Hello world</Box>
-        </ThemeProvider>,
-      );
-
-      const el = screen.getByText('Hello world');
-      expect(el).toHaveStyle(`${spacingProp}: 16px`);
+      const { container } = setup({ [spacingProp]: 4 });
+      expect(container.children[0]).toHaveStyle(`${spacingProp}: 16px`);
     },
   );
+
+  it.each(['color', 'cursor'])('does not render color or cursor props as HTML attributes', (prop) => {
+    const { container } = setup({ [prop]: 'something' });
+    expect(container).not.toHaveAttribute('color');
+    expect(container.children[0]).not.toHaveAttribute('color');
+  });
 });
