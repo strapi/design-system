86a193a79c1fa5f17c162f5a9b036940352b1bff
Mark Kaylor <mark.kaylor@strapi.io>
2023-02-02 10:32:50 +0100

    Remove useEffect from deprecated component

--- packages/strapi-design-system/src/Combobox/ComboboxList.js
+++ packages/strapi-design-system/src/Combobox/ComboboxList.js
@@ -1,24 +1,12 @@
-import { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { maintainScrollVisibility } from './utils';
 
 /**
  * @deprecated
  * This component was deprecated in 1.6.3. It will be removed in the next major release 2.0.0.
  */
 
-export const ComboboxList = ({ options, activeOptionRef }) => {
-  useEffect(() => {
-    if (activeOptionRef.current) {
-      maintainScrollVisibility(activeOptionRef.current);
-    }
-  }, [activeOptionRef]);
-
+export const ComboboxList = ({ options }) => {
   return options;
-};
-
-ComboboxList.defaultProps = {
-  activeOptionRef: undefined,
 };
 
 ComboboxList.propTypes = {
