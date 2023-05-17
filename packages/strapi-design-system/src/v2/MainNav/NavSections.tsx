0c6c4bcac4ea7d6e12f717bacbc5c94a492fc794
Minecraftschurli <minecraftschurli@gmail.com>
2023-05-17 10:00:44 +0200

    Convert v2/MainNav to Typescript

--- packages/strapi-design-system/src/v2/MainNav/NavSections.tsx
+++ packages/strapi-design-system/src/v2/MainNav/NavSections.tsx
@@ -1,17 +1,22 @@
 import React, { Children } from 'react';
 
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import { Box } from '../../Box';
-import { Flex } from '../../Flex';
+import { Flex, FlexProps } from '../../Flex';
 
 const BoxGrow = styled(Box)`
   flex-grow: 1;
   overflow-y: auto;
 `;
 
-export const NavSections = ({ children, spacing = 4, horizontal = false, ...props }) => {
+export interface NavSectionsProps extends FlexProps<'ul'> {
+  children: React.ReactNode;
+  spacing?: number;
+  horizontal?: boolean;
+}
+
+export const NavSections = ({ children, spacing = 4, horizontal = false, ...props }: NavSectionsProps) => {
   return (
     <BoxGrow paddingLeft={3} paddingRight={2} paddingTop={3} paddingBottom={8}>
       <Flex
@@ -28,8 +33,4 @@ export const NavSections = ({ children, spacing = 4, horizontal = false, ...prop
       </Flex>
     </BoxGrow>
   );
-};
-
-NavSections.propTypes = {
-  children: PropTypes.node.isRequired,
 };
