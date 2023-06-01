9c75a73c3460ef4e009d42db4efb51047fcad6e6
Gustav Hansen <gustav.hansen@strapi.io>
2023-06-01 13:31:20 +0200

    Fix: CarouselInput - fix slide rendering

--- packages/strapi-design-system/src/CarouselInput/CarouselSlide.tsx
+++ packages/strapi-design-system/src/CarouselInput/CarouselSlide.tsx
@@ -8,12 +8,14 @@ export interface CarouselSlideProps extends FlexProps {
 
 export const CarouselSlide = ({ label, children, selected = false, ...props }: CarouselSlideProps) => (
   <Flex
+    alignItems="center"
     display={selected ? 'flex' : 'none'}
     role="group"
     aria-roledescription="slide"
     aria-label={label}
     justifyContent="center"
     height="124px"
+    width="100%"
     {...props}
   >
     {children}
