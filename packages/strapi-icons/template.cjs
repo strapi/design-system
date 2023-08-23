146d2d5a90508186a2c22c6efd6bc2f86532d79d
Gustav Hansen <gustav.hansen@strapi.io>
2023-08-23 15:34:47 +0200

    flatten

--- packages/strapi-icons/template.cjs
+++ packages/strapi-icons/template.cjs
@@ -2,10 +2,11 @@ function template(
   { imports, interfaces, componentName, props, jsx, exports },
   { tpl }
 ) {
-  const iconName = componentName + 'Component';
+  const iconName = componentName + 'SVG';
   const styleName = componentName + 'Wrapper';
+  const exportName = componentName + 'Export';
   const svgIconComponent = '<' + iconName + ' {...props} />';
-  const wrapperComponentStart = '<' + styleName + ' colors={colors}>';
+  const wrapperComponentStart = '<' + styleName + ' color={color} colors={colors}>';
   const wrapperComponentEnd = '</' + styleName + '>';
   const toRender = wrapperComponentStart + svgIconComponent + wrapperComponentEnd;
 
@@ -13,21 +14,23 @@ function template(
     import * as React from 'react';
     import styled from 'styled-components';
 
-    const ${styleName} = styled.div\`
-      \${({ theme, color }) => \`
-        color: \${theme.colors[color]};
-      \`};
-
-      \${({ theme, colors }) => \`
-        \${Object.entries(colors).map(([name, value], index) => \`
-          --var-color-\${color.name}: \${theme.colors[value]};
-        \`)}
-      \`};
-    \`;
-
     const ${iconName} = (props: React.SVGProps<SVGSVGElement>) => ${jsx};
 
-    export default ({ colors, color, ...props }) => (${toRender});
+    const ${styleName} = styled(${iconName})\`
+      \${({ theme, color }) => \`
+    color: \${theme.colors[color]};
+  \`};
+
+      \${({ theme, colors }) => \`
+    \${Object.entries(colors).map(([name, value]) => \`
+      --var-color-\${name}: \${theme.colors[value]};
+    \`)}
+  \`};
+    \`;
+
+    const ${exportName} = ({ colors, color, ...props }: { colors?: object; color?: string }) => (${toRender});
+
+    export default ${exportName};
   `;
 }
 
