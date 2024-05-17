01ff7ca11ad6cc6b7e35b7c336705aee66ab4a0a
Josh <37798644+joshuaellis@users.noreply.github.com>
2024-05-17 15:19:11 +0100

    fix(design-system): edit Divider accessibility design

--- packages/design-system/src/components/Divider/Divider.tsx
+++ packages/design-system/src/components/Divider/Divider.tsx
@@ -1,17 +1,33 @@
+import * as React from 'react';
+
 import { styled } from 'styled-components';
 
 import { PropsToTransientProps } from '../../types';
 import { Box, BoxComponent, BoxProps } from '../Box';
 
-export interface DividerProps extends Omit<BoxProps<'hr'>, 'as' | 'background'> {}
+type DividerElement = HTMLDivElement;
 
-const DividerWrapper = styled<BoxComponent<'hr'>>(Box)<PropsToTransientProps<DividerProps>>`
+interface DividerProps extends Omit<BoxProps<'div'>, 'tag'> {}
+
+const Divider = React.forwardRef<DividerElement, DividerProps>((props, forwardedRef) => {
+  return (
+    <DividerImpl
+      ref={forwardedRef}
+      background="neutral150"
+      {...props}
+      data-orientation="horizontal"
+      role="separator"
+      tag="div"
+    />
+  );
+});
+
+const DividerImpl = styled<BoxComponent<'div'>>(Box)<PropsToTransientProps<DividerProps>>`
   height: 1px;
   border: none;
   /* If contained in a Flex parent we want to prevent the Divider to shink */
   flex-shrink: 0;
 `;
 
-export const Divider = (props: DividerProps) => {
-  return <DividerWrapper {...props} background="neutral150" tag="hr" />;
-};
+export { Divider };
+export type { DividerElement, DividerProps };
