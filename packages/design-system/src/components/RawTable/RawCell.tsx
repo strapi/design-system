87c69e1dcf0cc622db56951575132be270381662
Madhuri Sandbhor <madhuri.sandbhor@strapi.io>
2024-06-20 15:06:54 +0200

    chore: forwardRefs are added to possible places

--- packages/design-system/src/components/RawTable/RawCell.tsx
+++ packages/design-system/src/components/RawTable/RawCell.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import { getFocusableNodes, getFocusableNodesWithKeyboardNav } from '../../helpers/getFocusableNodes';
 import { KeyboardKeys } from '../../helpers/keyboardKeys';
+import { useComposedRefs } from '../../hooks/useComposeRefs';
 import { useIsomorphicLayoutEffect } from '../../hooks/useIsomorphicLayoutEffect';
 import { Box, BoxProps } from '../Box';
 
@@ -20,162 +21,165 @@ interface RawTdProps extends BoxProps<'td' | 'th'> {
   };
 }
 
-const RawTd = ({ coords = { col: 0, row: 0 }, tag = 'td', ...props }: RawTdProps) => {
-  const tdRef = React.useRef<HTMLTableCellElement>(null!);
-  const { rowIndex, colIndex, setTableValues } = useTable();
-  const [isActive, setIsActive] = React.useState(false);
+const RawTd = React.forwardRef<HTMLTableCellElement, RawTdProps>(
+  ({ coords = { col: 0, row: 0 }, tag = 'td', ...props }, forwardedRef) => {
+    const tdRef = React.useRef<HTMLTableCellElement>(null!);
+    const composedRef = useComposedRefs(forwardedRef, tdRef);
+    const { rowIndex, colIndex, setTableValues } = useTable();
+    const [isActive, setIsActive] = React.useState(false);
 
-  const handleKeyDown: React.KeyboardEventHandler<HTMLTableCellElement> = (e) => {
-    const focusableNodes = getFocusableNodes(tdRef.current, true);
+    const handleKeyDown: React.KeyboardEventHandler<HTMLTableCellElement> = (e) => {
+      const focusableNodes = getFocusableNodes(tdRef.current, true);
 
-    /**
-     * If the cell does not have focusable children or if it has focusable children
-     * without keyboard navigation, we should not run the handler.
-     */
-    if (
-      focusableNodes.length === 0 ||
-      (focusableNodes.length === 1 && getFocusableNodesWithKeyboardNav(focusableNodes).length === 0)
-    ) {
-      return;
       /**
-       * This allows cells that **only** have buttons in them to still be
-       * navigable with the keyboard arrow keys (left / right) as if they were grid cells.
-       *
-       * If there are nextNodes (next child node) then we stop the table's keyboard navigation
-       * handlers from happening.
+       * If the cell does not have focusable children or if it has focusable children
+       * without keyboard navigation, we should not run the handler.
        */
-    }
-    if (focusableNodes.length > 1 && !focusableNodes.find((node) => node.tagName !== 'BUTTON')) {
-      e.preventDefault();
-      const focussedButtonIndex = focusableNodes.findIndex((node) => node === document.activeElement);
-
-      if (e.key === KeyboardKeys.RIGHT) {
-        const nextNode = focusableNodes[focussedButtonIndex + 1];
-
-        if (nextNode) {
-          e.stopPropagation();
-          nextNode.focus();
-        }
-      } else if (e.key === KeyboardKeys.LEFT) {
-        const nextNode = focusableNodes[focussedButtonIndex - 1];
-
-        if (nextNode) {
-          e.stopPropagation();
-          nextNode.focus();
-        }
+      if (
+        focusableNodes.length === 0 ||
+        (focusableNodes.length === 1 && getFocusableNodesWithKeyboardNav(focusableNodes).length === 0)
+      ) {
+        return;
+        /**
+         * This allows cells that **only** have buttons in them to still be
+         * navigable with the keyboard arrow keys (left / right) as if they were grid cells.
+         *
+         * If there are nextNodes (next child node) then we stop the table's keyboard navigation
+         * handlers from happening.
+         */
       }
+      if (focusableNodes.length > 1 && !focusableNodes.find((node) => node.tagName !== 'BUTTON')) {
+        e.preventDefault();
+        const focussedButtonIndex = focusableNodes.findIndex((node) => node === document.activeElement);
 
-      return;
-    }
+        if (e.key === KeyboardKeys.RIGHT) {
+          const nextNode = focusableNodes[focussedButtonIndex + 1];
 
-    const isEnterKey = e.key === KeyboardKeys.ENTER;
+          if (nextNode) {
+            e.stopPropagation();
+            nextNode.focus();
+          }
+        } else if (e.key === KeyboardKeys.LEFT) {
+          const nextNode = focusableNodes[focussedButtonIndex - 1];
 
-    if (isEnterKey && !isActive) {
-      setIsActive(true);
-      /**
-       * Cells should be "escapeable" with the escape key or enter key
-       */
-    } else if ((e.key === KeyboardKeys.ESCAPE || isEnterKey) && isActive) {
-      /**
-       * It's expected behaviour that the cell can't be escaped with `enter` if
-       * the element that is focussed is an anchor tag.
-       */
-      if (isEnterKey && document.activeElement?.tagName === 'A') {
+          if (nextNode) {
+            e.stopPropagation();
+            nextNode.focus();
+          }
+        }
+
         return;
       }
 
-      setIsActive(false);
-      tdRef.current.focus();
-    } else if (isActive) {
-      /**
-       * This stops the table navigation from working
-       */
-      e.stopPropagation();
-    }
-  };
+      const isEnterKey = e.key === KeyboardKeys.ENTER;
 
-  const isFocused = rowIndex === coords.row - 1 && colIndex === coords.col - 1;
-
-  /**
-   * Handles tabindex of the rendered cell element
-   */
-  useIsomorphicLayoutEffect(() => {
-    const focusableNodes = getFocusableNodes(tdRef.current, true);
-
-    /**
-     * We should focus the cell if there are no focussable children inside
-     * If there is only one focusable child and it has it's own keyboard navigation
-     * Or if there is more than one focusable child unless those children
-     * are exclusively buttons.
-     */
-    if (
-      focusableNodes.length === 0 ||
-      (focusableNodes.length === 1 && getFocusableNodesWithKeyboardNav(focusableNodes).length !== 0) ||
-      (focusableNodes.length > 1 && Boolean(focusableNodes.find((node) => node.tagName !== 'BUTTON')))
-    ) {
-      tdRef.current.setAttribute('tabIndex', !isActive && isFocused ? '0' : '-1');
-
-      focusableNodes.forEach((node, index) => {
-        node.setAttribute('tabIndex', isActive ? '0' : '-1');
-
+      if (isEnterKey && !isActive) {
+        setIsActive(true);
         /**
-         * When a cell is active we want to focus the
-         * first focusable element simulating a focus trap
+         * Cells should be "escapeable" with the escape key or enter key
          */
-        if (isActive && index === 0) {
-          node.focus();
+      } else if ((e.key === KeyboardKeys.ESCAPE || isEnterKey) && isActive) {
+        /**
+         * It's expected behaviour that the cell can't be escaped with `enter` if
+         * the element that is focussed is an anchor tag.
+         */
+        if (isEnterKey && document.activeElement?.tagName === 'A') {
+          return;
         }
-      });
-    } else {
-      focusableNodes.forEach((node) => {
-        node.setAttribute('tabIndex', isFocused ? '0' : '-1');
-      });
-    }
-  }, [isActive, isFocused]);
 
-  const handleFocusableNodeFocus = React.useCallback(() => {
-    const focusableNodes = getFocusableNodes(tdRef.current, true);
-
-    /**
-     * If there's 1 or more focusable children and at least one has keyboard navigation
-     * or the children are exclusively button elements the cell should be using the "active" system
-     */
-    if (
-      focusableNodes.length >= 1 &&
-      (getFocusableNodesWithKeyboardNav(focusableNodes).length !== 0 ||
-        !focusableNodes.find((node) => node.tagName !== 'BUTTON'))
-    ) {
-      setIsActive(true);
-    }
-    /**
-     * This function is wrapped in `useCallback` so we can safely
-     * assume that the reference will not change
-     */
-    setTableValues({ rowIndex: coords.row - 1, colIndex: coords.col - 1 });
-  }, [coords, setTableValues]);
-
-  /**
-   * This handles the case where you click on a focusable
-   * node that has it's own keyboard nav (e.g. Input)
-   */
-  useIsomorphicLayoutEffect(() => {
-    const cell = tdRef.current;
-    const focusableNodes = getFocusableNodes(cell, true);
-
-    focusableNodes.forEach((node) => {
-      node.addEventListener('focus', handleFocusableNodeFocus);
-    });
-
-    return () => {
-      const focusableNodes = getFocusableNodes(cell, true);
-      focusableNodes.forEach((node) => {
-        node.removeEventListener('focus', handleFocusableNodeFocus);
-      });
+        setIsActive(false);
+        tdRef.current.focus();
+      } else if (isActive) {
+        /**
+         * This stops the table navigation from working
+         */
+        e.stopPropagation();
+      }
     };
-  }, [handleFocusableNodeFocus]);
 
-  return <Box role="gridcell" tag={tag} ref={tdRef} onKeyDown={handleKeyDown} {...props} />;
-};
+    const isFocused = rowIndex === coords.row - 1 && colIndex === coords.col - 1;
+
+    /**
+     * Handles tabindex of the rendered cell element
+     */
+    useIsomorphicLayoutEffect(() => {
+      const focusableNodes = getFocusableNodes(tdRef.current, true);
+
+      /**
+       * We should focus the cell if there are no focussable children inside
+       * If there is only one focusable child and it has it's own keyboard navigation
+       * Or if there is more than one focusable child unless those children
+       * are exclusively buttons.
+       */
+      if (
+        focusableNodes.length === 0 ||
+        (focusableNodes.length === 1 && getFocusableNodesWithKeyboardNav(focusableNodes).length !== 0) ||
+        (focusableNodes.length > 1 && Boolean(focusableNodes.find((node) => node.tagName !== 'BUTTON')))
+      ) {
+        tdRef.current.setAttribute('tabIndex', !isActive && isFocused ? '0' : '-1');
+
+        focusableNodes.forEach((node, index) => {
+          node.setAttribute('tabIndex', isActive ? '0' : '-1');
+
+          /**
+           * When a cell is active we want to focus the
+           * first focusable element simulating a focus trap
+           */
+          if (isActive && index === 0) {
+            node.focus();
+          }
+        });
+      } else {
+        focusableNodes.forEach((node) => {
+          node.setAttribute('tabIndex', isFocused ? '0' : '-1');
+        });
+      }
+    }, [isActive, isFocused]);
+
+    const handleFocusableNodeFocus = React.useCallback(() => {
+      const focusableNodes = getFocusableNodes(tdRef.current, true);
+
+      /**
+       * If there's 1 or more focusable children and at least one has keyboard navigation
+       * or the children are exclusively button elements the cell should be using the "active" system
+       */
+      if (
+        focusableNodes.length >= 1 &&
+        (getFocusableNodesWithKeyboardNav(focusableNodes).length !== 0 ||
+          !focusableNodes.find((node) => node.tagName !== 'BUTTON'))
+      ) {
+        setIsActive(true);
+      }
+      /**
+       * This function is wrapped in `useCallback` so we can safely
+       * assume that the reference will not change
+       */
+      setTableValues({ rowIndex: coords.row - 1, colIndex: coords.col - 1 });
+    }, [coords, setTableValues]);
+
+    /**
+     * This handles the case where you click on a focusable
+     * node that has it's own keyboard nav (e.g. Input)
+     */
+    useIsomorphicLayoutEffect(() => {
+      const cell = tdRef.current;
+      const focusableNodes = getFocusableNodes(cell, true);
+
+      focusableNodes.forEach((node) => {
+        node.addEventListener('focus', handleFocusableNodeFocus);
+      });
+
+      return () => {
+        const focusableNodes = getFocusableNodes(cell, true);
+        focusableNodes.forEach((node) => {
+          node.removeEventListener('focus', handleFocusableNodeFocus);
+        });
+      };
+    }, [handleFocusableNodeFocus]);
+
+    return <Box role="gridcell" tag={tag} ref={composedRef} onKeyDown={handleKeyDown} {...props} />;
+  },
+);
 
 /* -------------------------------------------------------------------------------------------------
  * RawTh
--- packages/design-system/src/components/SimpleMenu/SimpleMenu.tsx
+++ packages/design-system/src/components/SimpleMenu/SimpleMenu.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import { stripReactIdOfColon } from '../../helpers/strings';
+import { useComposedRefs } from '../../hooks/useComposeRefs';
 import { useId } from '../../hooks/useId';
 import { useIntersection } from '../../hooks/useIntersection';
 
@@ -23,51 +24,57 @@ interface SimpleMenuProps
   onReachEnd?: (entry: IntersectionObserverEntry) => void;
 }
 
-const SimpleMenu = ({ children, onOpen, onClose, popoverPlacement, onReachEnd, ...props }: SimpleMenuProps) => {
-  /**
-   * Used for the intersection observer
-   */
-  const contentRef = React.useRef<HTMLDivElement>(null);
-
-  const [internalIsOpen, setInternalIsOpen] = React.useState(false);
-
-  const handleReachEnd = (entry: IntersectionObserverEntry) => {
-    if (onReachEnd) {
-      onReachEnd(entry);
-    }
-  };
-
-  const handleOpenChange = (isOpen: boolean) => {
-    if (isOpen && typeof onOpen === 'function') {
-      onOpen();
-    } else if (!isOpen && typeof onClose === 'function') {
-      onClose();
-    }
-
-    setInternalIsOpen(isOpen);
-  };
-
-  const generatedId = useId();
-  const intersectionId = `intersection-${stripReactIdOfColon(generatedId)}`;
-
-  useIntersection(contentRef, handleReachEnd, {
-    selectorToWatch: `#${intersectionId}`,
+const SimpleMenu = React.forwardRef<HTMLButtonElement, SimpleMenuProps>(
+  ({ children, onOpen, onClose, popoverPlacement, onReachEnd, ...props }, forwardedRef) => {
+    const triggerRef = React.useRef<HTMLButtonElement>(null);
+    const composedRef = useComposedRefs(forwardedRef, triggerRef);
     /**
-     * We need to know when the select is open because only then will viewportRef
-     * not be null. Because it uses a portal that (sensibly) is not mounted 24/7.
+     * Used for the intersection observer
      */
-    skipWhen: !internalIsOpen,
-  });
+    const contentRef = React.useRef<HTMLDivElement>(null);
 
-  return (
-    <Menu.Root onOpenChange={handleOpenChange}>
-      <Menu.Trigger {...props}>{props.label}</Menu.Trigger>
-      <Menu.Content intersectionId={intersectionId} popoverPlacement={popoverPlacement}>
-        {children}
-      </Menu.Content>
-    </Menu.Root>
-  );
-};
+    const [internalIsOpen, setInternalIsOpen] = React.useState(false);
+
+    const handleReachEnd = (entry: IntersectionObserverEntry) => {
+      if (onReachEnd) {
+        onReachEnd(entry);
+      }
+    };
+
+    const handleOpenChange = (isOpen: boolean) => {
+      if (isOpen && typeof onOpen === 'function') {
+        onOpen();
+      } else if (!isOpen && typeof onClose === 'function') {
+        onClose();
+      }
+
+      setInternalIsOpen(isOpen);
+    };
+
+    const generatedId = useId();
+    const intersectionId = `intersection-${stripReactIdOfColon(generatedId)}`;
+
+    useIntersection(contentRef, handleReachEnd, {
+      selectorToWatch: `#${intersectionId}`,
+      /**
+       * We need to know when the select is open because only then will viewportRef
+       * not be null. Because it uses a portal that (sensibly) is not mounted 24/7.
+       */
+      skipWhen: !internalIsOpen,
+    });
+
+    return (
+      <Menu.Root onOpenChange={handleOpenChange}>
+        <Menu.Trigger ref={composedRef} {...props}>
+          {props.label}
+        </Menu.Trigger>
+        <Menu.Content ref={contentRef} intersectionId={intersectionId} popoverPlacement={popoverPlacement}>
+          {children}
+        </Menu.Content>
+      </Menu.Root>
+    );
+  },
+);
 
 const MenuItem = Menu.Item;
 type MenuItemProps = Menu.ItemProps;
