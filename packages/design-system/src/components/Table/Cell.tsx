61936d63f6b7eb390b351138c48572442d5b1e4c
Madhuri Sandbhor <madhuri.sandbhor@strapi.io>
2024-06-25 12:01:47 +0200

    chore: standardise forwardRefs across components (#1748)

--- packages/design-system/src/components/Table/Cell.tsx
+++ packages/design-system/src/components/Table/Cell.tsx
@@ -26,21 +26,21 @@ export interface ThProps extends RawTdProps {
   action?: React.ReactNode;
 }
 
-export const Th = ({ children, action, ...props }: ThProps) => {
+export const Th = React.forwardRef<HTMLTableCellElement, ThProps>(({ children, action, ...props }, forwardedRef) => {
   return (
-    <CellWrapper color="neutral600" as={RawTh} {...props}>
+    <CellWrapper color="neutral600" as={RawTh} ref={forwardedRef} {...props}>
       <Flex>
         {children}
         {action}
       </Flex>
     </CellWrapper>
   );
-};
+});
 
-export const Td = ({ children, ...props }: RawTdProps) => {
+export const Td = React.forwardRef<HTMLTableCellElement, RawTdProps>(({ children, ...props }, forwardedRef) => {
   return (
-    <CellWrapper color="neutral800" {...props}>
+    <CellWrapper color="neutral800" ref={forwardedRef} {...props}>
       {children}
     </CellWrapper>
   );
-};
+});
