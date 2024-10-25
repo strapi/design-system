2dca53c4b781bdee0b36d8ffac41603debce9a94
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2024-10-25 17:52:33 +0200

    enhancement: add xs size to status component

--- packages/design-system/src/components/Status/Status.tsx
+++ packages/design-system/src/components/Status/Status.tsx
@@ -6,7 +6,7 @@ import { Box, BoxProps } from '../../primitives/Box';
 import { Flex } from '../../primitives/Flex';
 
 type StatusVariant = 'alternative' | 'danger' | 'neutral' | 'primary' | 'secondary' | 'success' | 'warning';
-type StatusSize = 'S' | 'M';
+type StatusSize = 'XS' | 'S' | 'M';
 
 interface StatusProps extends BoxProps {
   variant?: StatusVariant;
@@ -19,14 +19,26 @@ interface StatusProps extends BoxProps {
   children: React.ReactNode;
 }
 
+const getPadding = (size: StatusSize): { paddingX: BoxProps['paddingTop']; paddingY: BoxProps['paddingLeft'] } => {
+  if (size === 'XS') {
+    return { paddingX: '0.6rem', paddingY: '0.2rem' };
+  }
+
+  if (size === 'S') {
+    return { paddingX: 2, paddingY: 1 };
+  }
+
+  // Size M
+  return { paddingX: 5, paddingY: 4 };
+};
+
 const Status = ({ variant = 'primary', showBullet = true, size = 'M', children, ...props }: StatusProps) => {
   const backgroundColor = `${variant}100` satisfies keyof DefaultTheme['colors'];
   const borderColor = `${variant}200` satisfies keyof DefaultTheme['colors'];
   const bulletColor = `${variant}600` satisfies keyof DefaultTheme['colors'];
   const textColor = `${variant}600` satisfies keyof DefaultTheme['colors'];
 
-  const paddingX = size === 'S' ? 2 : 5;
-  const paddingY = size === 'S' ? 1 : 4;
+  const { paddingX, paddingY } = getPadding(size);
 
   return (
     <Box
