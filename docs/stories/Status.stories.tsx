bcc2862e3d966394c2d94fbabb3901fb307496d8
Rémi de Juvigny <8087692+remidej@users.noreply.github.com>
2024-10-28 11:19:10 +0100

    enhancement: add xs size to status component (#1801)

--- docs/stories/Status.stories.tsx
+++ docs/stories/Status.stories.tsx
@@ -52,3 +52,27 @@ export const SizeS = {
 
   name: 'size S',
 } satisfies Story;
+
+export const SizeXS = {
+  render: () => (
+    <Flex direction="column" alignItems="stretch" gap={3}>
+      <Status variant="success" size="XS" showBullet={false}>
+        <Typography fontWeight="bold" textColor="success700">
+          Published
+        </Typography>
+      </Status>
+      <Status variant="secondary" size="XS" showBullet={false}>
+        <Typography fontWeight="bold" textColor="secondary700">
+          Draft
+        </Typography>
+      </Status>
+      <Status variant="alternative" size="XS" showBullet={false}>
+        <Typography fontWeight="bold" textColor="alternative700">
+          Updated
+        </Typography>
+      </Status>
+    </Flex>
+  ),
+
+  name: 'size XS',
+} satisfies Story;
