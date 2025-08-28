3a06c6eb940204a861db3ddb643bd0659b22c645
Adrien Lepoutre <thewebsdoor@gmail.com>
2025-08-28 18:08:59 +0200

    fix: resizable and minHeight controlable as properties

--- packages/design-system/src/components/Textarea/Textarea.tsx
+++ packages/design-system/src/components/Textarea/Textarea.tsx
@@ -11,10 +11,22 @@ interface TextareaProps
     Pick<Field.InputProps, 'hasError' | 'id' | 'name' | 'required'> {
   value?: string;
   'aria-describedby'?: string;
+  resizable?: boolean;
 }
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ disabled, hasError: hasErrorProp, id: idProp, name: nameProp, required: requiredProp, ...props }, ref) => {
+  (
+    {
+      disabled,
+      hasError: hasErrorProp,
+      id: idProp,
+      name: nameProp,
+      required: requiredProp,
+      resizable = true,
+      ...props
+    },
+    ref,
+  ) => {
     const { error, ...field } = useField('Textarea');
     const hasError = Boolean(error) || hasErrorProp;
     const id = field.id ?? idProp;
@@ -46,6 +58,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
           id={id}
           name={name}
           aria-describedby={ariaDescription}
+          $resizable={resizable}
           {...props}
         />
       </Wrapper>
@@ -60,8 +73,8 @@ const Wrapper = styled<BoxComponent>(Box)<{ $hasError?: boolean }>`
 const TextareaElement = styled<BoxComponent<'textarea'>>(Box)`
   display: block;
   border: none;
-  resize: vertical;
-  min-height: 10.5rem;
+  resize: ${({ $resizable }) => ($resizable ? 'vertical' : 'none')};
+  min-height: ${({ minHeight }) => minHeight || '10.5rem'};
 
   &::placeholder {
     color: ${({ theme }) => theme.colors.neutral600};
